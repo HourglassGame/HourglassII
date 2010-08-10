5c436fb85951755cea62638989aed6d2b02a01cb
Evan Wallace <onlyone@senet.com.au>
2010-08-10 13:14:08 +1000

    Compatibility with latest 2.1

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
-#define BOOST_SP_DISABLE_THREADS
+
 #include <SFML/Graphics.hpp>
 #include <boost/thread.hpp>
 #include "TimeEngine.h"
@@ -32,7 +32,6 @@ namespace hg {
 ////////////////////////////////////////////////////////////
 int main()
 {
-    // Create main window
     RenderWindow App(VideoMode(640, 480), "Hourglass II");
     
     vector<vector<bool> > wall(MakeWall());
@@ -40,13 +39,10 @@ int main()
     
     ::boost::posix_time::time_duration stepTime(0,0,0,boost::posix_time::time_duration::ticks_per_second()/60);
     
-    // Start game loop
     while (App.IsOpened())
     {
-        //Wait for step
         ::boost::posix_time::ptime startTime(boost::posix_time::microsec_clock::universal_time());
         
-        //Load up input
         Event event;
         while (App.GetEvent(event))
         {
@@ -58,16 +54,13 @@ int main()
                     break;
             }
         }
-        
         const ::hg::Input input(App.GetInput());
+        
         cout << "called from main" << endl;
-        //Get result from time-engine
         ObjectList frame(timeEngine.getNextPlayerFrame(input.AsInputList()));
         
-        //Draw result
         Draw(App, frame, wall);
-            
-        //Wait for next step
+
         while (::boost::posix_time::microsec_clock::universal_time() - startTime < stepTime) {
             ::boost::this_thread::sleep(boost::posix_time::milliseconds(1));
         }
@@ -91,7 +84,17 @@ void ::hg::DrawWall(sf::RenderTarget& target, const std::vector<std::vector<bool
     for(unsigned int i = 0; i < wall.size(); ++i) {
         for(unsigned int j = 0; j < wall.at(i).size(); ++j) {
             if (wall.at(i).at(j)) {
-                target.Draw(Shape::Rectangle(32*i, 32*j, 32*(i+1), 32*(j+1), sf::Color()));
+                target.Draw
+                (
+                    Shape::Rectangle
+                    (
+                        32*static_cast<unsigned int>(i), 
+                        32*static_cast<unsigned int>(j),
+                        32*(static_cast<unsigned int>(i)+1),
+                        32*(static_cast<unsigned int>(j)+1),
+                        Color()
+                    )
+                );
             }
         }
     }
@@ -111,7 +114,7 @@ void ::hg::DrawBoxes(RenderTarget& target, const vector<Box>& boxList)
 void ::hg::DrawGuys(RenderTarget& target, const vector<Guy>& guyList)
 {
     foreach(const Guy& guy, guyList) {
-        target.Draw(sf::Shape::Rectangle(guy.getX()/100,
+        target.Draw(Shape::Rectangle(guy.getX()/100,
                                          guy.getY()/100,
                                          (guy.getX()+ guy.getWidth())/100,
                                          (guy.getY()+guy.getHeight())/100,
@@ -180,21 +183,14 @@ vector<vector<bool> > hg::MakeWall()
             actualWall.at(j).at(i) = wall.at(i).at(j);
         }
     }
-    //Logger::GetLogger().Log("Made Wall", loglevel::FINE);
     return actualWall;
 }
 
 TimeEngine hg::MakeTimeEngine(vector<vector<bool> >& wall)
 {
-    TimeEngine newEngine(5400,wall,3200,50);
     ObjectList newObjectList;
     newObjectList.addBox(Box(6400, 25600, 0 ,0, 3200, FORWARDS));
-    //int nX, int nY, int nXspeed, int nYspeed,int nWidth, int nHeight, bool nBoxCarrying, 
-    //int nBoxCarrySize, hg::TimeDirection nBoxCarryDirection, hg::TimeDirection nTimeDirection, 
-    //int nRelativeIndex, int nSubimage
-    
     newObjectList.addGuy(Guy(22000, 6400, 0, 0, 1600, 3200, false, 0, PAUSE, FORWARDS, 0, 0));
-    newEngine.checkConstistencyAndPropagateLevel(newObjectList,0);
-    return newEngine;
+    return TimeEngine(5400,wall,3200,50,newObjectList,0);
 }
 
