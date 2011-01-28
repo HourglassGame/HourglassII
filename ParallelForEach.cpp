7a59dee1c5d53281736a10819fd17806d843e067
Evan Wallace <onlyone@senet.com.au>
2011-01-28 14:31:36 +0000

    Added forgotten but critical files.

---
+++ ParallelForEach.cpp
@@ -0,0 +1,11 @@
+/*
+ *  ParallelForEach.cpp
+ *  HourglassSetupTest
+ *
+ *  Created by Evan Wallace on 27/01/11.
+ *  Copyright 2011 Team Causality. All rights reserved.
+ *
+ */
+
+#include "ParallelForEach.h"
+
