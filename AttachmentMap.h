bec989938ccc6a4afa9987197ac8315da8660d47
Evan Wallace <onlyone@senet.com.au>
2011-04-28 14:31:34 +0000

    Improved some comments.

--- AttachmentMap.h
+++ AttachmentMap.h
@@ -20,8 +20,11 @@ struct Attachment
             yOffset(nyOffset)
     {
     }
+    //index of platform to which thing is attached
     std::size_t platformIndex;
+    //offset to apply to platform position to get x position of attached thing
     int xOffset;
+    //offset to apply to platform position to get y position of attached thing
     int yOffset;
 };
 
@@ -47,9 +50,6 @@ public:
 private:
     //Lists of attachments:
     //buttonAttachments[n] contains the Attachment information for the button with index n
-    //Attachment information is:
-    //platformIndex -- index of platform to which button is attached
-    //(x/y)Offset --  offset to apply to platform position to get button position
     std::vector<Attachment> buttonAttachments;
     //See above -- portalAttachments are the same.
     std::vector<Attachment> portalAttachments;
