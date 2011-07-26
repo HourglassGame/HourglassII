e2f2f4e90171b4154c1c6859ff4979999a147168
Evan Wallace <onlyone@senet.com.au>
2011-07-26 21:51:36 +0000

    BUILD BREAKING CHANGE! You need to now also link to tbbmalloc. Changed custom allocator from nedmalloc to tbb_malloc (due to a memory leak issue that I recently discovered (and moments ago found out how to fix)). I think tbbmalloc is faster in any case. Nedmalloc will live on in our hearts (and may come back later...).

--- multi_thread_allocator.h
+++ multi_thread_allocator.h
@@ -1,12 +1,22 @@
 #ifndef HG_MULTI_THREAD_ALLOCATOR_H
 #define HG_MULTI_THREAD_ALLOCATOR_H
-#include "ned_allocator.h"
+//The purpose of this file is to provide a single place that needs to be changed
+//to modify the memory allocation used in the regions of the code that execute concurrently.
+
+#include <tbb/scalable_allocator.h>
 namespace hg {
+    //Metafunction to calculate the allocator type to use
+    //when multiple threads may be performing allocations/deallocations
+    //simultaneously.
     template<typename T> struct multi_thread_allocator {
-        //The allocator type to use in cases where there
-        //are many threads simultaneously wanting
-        //to perform allocation/deallocation.
-        typedef nedalloc::ned_allocator<T> type;
+        typedef tbb::scalable_allocator<T> type;
     };
+    //Versions of the C library functions to use
+    //when multiple threads may be performing allocations/deallocations
+    //simultaneously.
+    inline void* multi_thread_malloc(std::size_t size) { return scalable_malloc(size); }
+    inline void  multi_thread_free(void* p) { return scalable_free(p); }
+    inline void* multi_thread_calloc(size_t n, size_t size) { return scalable_calloc(n, size); }
+    inline void* multi_thread_realloc(void* p, size_t size) { return scalable_realloc(p, size); }
 }
 #endif //HG_MULTI_THREAD_ALLOCATOR_H
