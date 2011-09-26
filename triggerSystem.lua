1cc085ce3107ee514d547d6ca13a5f6a9f4aa898
GoogleFrog <googl3frog@gmail.com>
2011-09-26 15:27:59 +0000

    Replaced frameNumber hax with inbuilt functionality.

--- triggerSystem.lua
+++ triggerSystem.lua
@@ -315,15 +315,13 @@ return {
     calculatePhysicsAffectingStuff = function(self, frameNumber, triggerArrivals)
         local retv = {}
         
-        self.outputTriggers[3] = {triggerArrivals[3][1] + 1}
-        
         retv.additionalBoxes = {}
         
-        if triggerArrivals[3][1]%300 == 0 then
+        if frameNumber%300 == 0 then
             retv.additionalBoxes[#retv.additionalBoxes+1] = {x = 6400, y = 6400, xspeed = 0, yspeed = 0, size = 3200, illegalPortal = nil, arrivalBasis = nil, timeDirection = "forwards"}
         end
         
-        if triggerArrivals[3][1]%200 == 0 then
+        if frameNumber%200 == 0 then
             retv.additionalBoxes[#retv.additionalBoxes+1] = {x = 12800, y = 6400, xspeed = 600, yspeed = -400, size = 3200, illegalPortal = nil, arrivalBasis = nil, timeDirection = "forwards"}
         end
         
