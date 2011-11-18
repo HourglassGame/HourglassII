73fcf3caabb36873948840e74d36da5baa0d4db3
evanwallace <evanwallace@5321f17f-e843-4a89-a37a-4f8a1ec584ce>
2011-11-18 10:58:22 +0000

    Fixed eclipse parser workaround.

--- trunk/Foreach.h
+++ trunk/Foreach.h
@@ -2,7 +2,7 @@
 #define HG_FOREACH_H
 #include <boost/foreach.hpp>
 
-#ifdef ECLIPSE_PARSER
+#ifdef __CDT_PARSER__
     //Be aware that this could be problematic if a or b are themselves macros
     #define foreach(a, b) for(a : b)
 #else
