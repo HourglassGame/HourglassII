5803bff6e4d7a96de3e2e43b6ebf85cc82556912
Evan Wallace <onlyone@senet.com.au>
2011-11-19 11:49:52 +0000

    Made windows ci agent manually use private key.

--- ci_agent_windows.py
+++ ci_agent_windows.py
@@ -20,6 +20,7 @@ def upload(revision):
     #scp file
     subprocess.call(
         ["F:/Program Files/PuTTY/pscp.exe"]
+        + ["-i", "F:/User~Docs/Keven/Workspace/ssh_key/Sourceforge.ppk"]
         + [release_filename]
         + ["evanwallace,hourglassii@frs.sourceforge.net"
            +":/home/frs/project/h/ho/hourglassii/Release/Windows/"])
