3d65ed662732aea150d9c2486d4550bfe7621f2b
Evan Wallace <onlyone@senet.com.au>
2011-11-19 04:56:18 +0000

    Added windows ci agent.

---
+++ ci_agent_windows.py
@@ -0,0 +1,64 @@
+import build_windows
+import sys
+import time
+import re
+import subprocess
+import shutil
+
+def build():
+    build_windows.main()
+    pass
+
+def upload(revision):
+    release_filename = "build/HourglassIIr"+str(revision)+".7z"
+    #rename file
+    shutil.copy(
+        "build/HourglassII.7z",
+         release_filename)
+    #scp file
+    subprocess.call(
+        ["F:/Program Files/PuTTY/pscp.exe"]
+        + ["-batch"]
+        + ["-i","F:/User~Docs/Keven/Workspace/ssh_key/id_dsa"]
+        + [release_filename]
+        + ["evanwallace,hourglassii@frs.sourceforge.net"
+           +":/home/frs/project/h/ho/hourglassii/Release/Windows/"])
+
+
+def svn_up():
+    print("Updating...")
+    output = subprocess.check_output(["svn"] + ["up"]).decode("UTF-8").replace("\r\n", "\n")
+    print(output)
+    print("Updated to revision",
+      re.search(r"^((At revision )|(Updated to revision ))([0-9]*)\.$", output, re.MULTILINE).group(4))
+
+def svnversion():
+    print("Getting current working version...")
+    
+    output = subprocess.check_output(["svn", "info"]).decode("UTF-8")
+    version = int(
+      re.search(r"Last Changed Rev: ([0-9]*)", output).group(1))
+    print("Current working version is:", version)
+    return version
+
+current_revision = svnversion()
+while True:
+    try:
+        svn_up()
+        revision = svnversion()
+        if revision > current_revision:
+            current_revision = revision
+            print("Building revision", current_revision, "...")
+            build()
+            print("Finished building, uploading...")
+            upload(current_revision)
+            shutil.rmtree("build")
+            print("Finished uploading.")
+        else:
+            print("Going to sleep.")
+            time.sleep(60)
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except:
+        print("Unexpected error:", sys.exc_info())
+        pass
