ad8c43ab5b2b388d16ec5d8b63cf278854189ccf
Evan Wallace <onlyone@senet.com.au>
2011-12-03 02:06:06 +0000

    Refactored replay I/O into a separate file, and added a simple test case.

--- ReplayIO.cpp
+++ ReplayIO.cpp
@@ -1,6 +1,29 @@
 #include "ReplayIO.h"
-#include <iostream>
+#include <fstream>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/serialization/vector.hpp>
 namespace hg {
-    void saveReplay(std::string const& filename, std::vector<InputList> const& replay);
-    std::vector<InputList> loadReplay(std::string const& filename);
+    void saveReplay(std::string const& filename, std::vector<InputList> const& replay)
+    {
+        std::ofstream ofs(filename.c_str());
+        saveReplay(ofs, replay);
+    }
+    std::vector<InputList> loadReplay(std::string const& filename)
+    {
+        std::ifstream ifs(filename.c_str());
+        return ifs.is_open() ? loadReplay(ifs) : std::vector<InputList>();
+    }
+    void saveReplay(std::ostream& outputStream, std::vector<InputList> const& replay)
+    {
+        boost::archive::text_oarchive oa(outputStream);
+        oa << replay;
+    }
+    std::vector<InputList> loadReplay(std::istream& inputStream)
+    {
+        std::vector<InputList> replay;
+        boost::archive::text_iarchive ia(inputStream);
+        ia >> replay;
+        return replay;
+    }
 }
