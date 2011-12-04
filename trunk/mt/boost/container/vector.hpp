740daefae33853de2b72585fa6c1231486c0eedd
evanwallace <evanwallace@5321f17f-e843-4a89-a37a-4f8a1ec584ce>
2011-12-04 06:28:05 +0000

    Added missing files

---
+++ trunk/mt/boost/container/vector.hpp
@@ -0,0 +1,21 @@
+#ifndef HG_MT_BOOST_CONTAINER_VECTOR
+#define HG_MT_BOOST_CONTAINER_VECTOR
+#include "../../../multi_thread_allocator.h"
+#include <boost/container/vector.hpp>
+namespace hg {
+namespace mt {
+namespace boost{
+namespace container {
+template<typename T>
+struct vector {
+	typedef
+		::boost::container::vector<
+			T,
+			typename multi_thread_allocator<T>::type
+		> type;
+};
+}
+}
+}
+}
+#endif //HG_MT_BOOST_CONTAINER_VECTOR
