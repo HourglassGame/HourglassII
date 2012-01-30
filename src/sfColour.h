377cb674af2fad3bcb21a252a7d011e82c1b5f94
Evan Wallace <onlyone@senet.com.au>
2012-01-30 17:56:29 +0000

    Fixed problematic declaration of `interpretAsColour`.

--- src/sfColour.h
+++ src/sfColour.h
@@ -3,7 +3,7 @@
 #include <SFML/Graphics/Color.hpp>
 namespace hg {
     //Interprets colour as |...|RRRRRRRR|GGGGGGGG|BBBBBBBB|--------|
-    sf::Color interpretAsColour(unsigned colour)
+    inline sf::Color interpretAsColour(unsigned colour)
     {
         return sf::Color((colour & 0xFF000000) >> 24, (colour & 0xFF0000) >> 16, (colour & 0xFF00) >> 8);
     }
