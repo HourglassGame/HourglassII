5cbed193caba78c74e504851c80b874f1d02d4d5
evanwallace <evanwallace@5321f17f-e843-4a89-a37a-4f8a1ec584ce>
2012-02-25 14:16:40 +0000

    Fixed stupid move_function bug properly. Added test.

---
+++ trunk/src/move_function_test.cpp
@@ -0,0 +1,33 @@
+#include "move_function.h"
+#include "TestDriver.h"
+namespace hg {
+namespace move_function_test {
+namespace {
+    bool testOperatorBool()
+    {
+        bool passed(true);
+        move_function<void()> a;
+        passed = passed && !a;
+        if (a) {
+            passed = false;
+        }
+        a = testOperatorBool;
+        passed = passed && a;
+        if (a) {
+            //No problem
+        }
+        else {
+            passed = false;
+        }
+        return passed;
+    }
+
+    struct tester {
+        tester() {
+            ::hg::getTestDriver().registerUnitTest(testOperatorBool);
+        }
+    } tester;
+}
+}
+}
+
