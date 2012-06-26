9d1d4e2635c7d41a012a7af05871e76fe8067b07
evanwallace <evanwallace@5321f17f-e843-4a89-a37a-4f8a1ec584ce>
2012-06-26 17:36:50 +0000

    Removed parallel time engine execution (for debug purposes).

--- trunk/src/ParallelForEach.h
+++ trunk/src/ParallelForEach.h
@@ -3,7 +3,7 @@
 #include <boost/range.hpp>
 #include <tbb/parallel_do.h>
 #include "TBBExceptionWrapper.h"
-//#include <boost/range/algorithm/for_each.hpp>
+#include <boost/range/algorithm/for_each.hpp>
 namespace hg {
 namespace detail {
     //Const and non-const version to allow both const ranges and modifying Functions
@@ -12,15 +12,15 @@ namespace detail {
         RandomAccessRange& range, Func func, tbb::task_group_context& context)
     {
         
-        tbb::parallel_do(boost::begin(range), boost::end(range), func, context);
-        //boost::for_each(range, func);
+        //tbb::parallel_do(boost::begin(range), boost::end(range), func, context);
+        boost::for_each(range, func);
     }
     template<typename RandomAccessRange, typename Func>
     void parallel_for_each(
         RandomAccessRange const& range, Func func, tbb::task_group_context& context)
     {
-        tbb::parallel_do(boost::begin(range), boost::end(range), func, context);
-        //boost::for_each(range, func);
+        //tbb::parallel_do(boost::begin(range), boost::end(range), func, context);
+        boost::for_each(range, func);
     }
     template<typename RandomAccessRange, typename Func>
     struct parallel_for_each_struct {
