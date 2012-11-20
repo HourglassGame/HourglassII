1c1e31c8911b2373b63eb4beda0bd66406bf6c4b
evanwallace <evanwallace@5321f17f-e843-4a89-a37a-4f8a1ec584ce>
2012-11-20 05:19:11 +0000

    Removed RetardedNotActuallyAGlitzGlitz and BasicConfiguredTriggerSystem

--- trunk/src/PhysicsEngineUtilities.h
+++ trunk/src/PhysicsEngineUtilities.h
@@ -174,12 +174,7 @@ bool IntersectingRectanglesInclusive(int x1, int y1, int w1, int h1, int x2, int
 bool IntersectingRectanglesExclusive(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2);
 bool IntersectingRectanglesInclusiveCollisionOverlap(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2, int buffer);
 bool RectangleWithinInclusive(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2);
-FrameView makeFrameView(
-    mt::std::vector<GuyOutputInfo>::type const& guyInfo,
-    mt::std::vector<RetardedNotActuallyAGlitzGlitz>::type const& backgroundTriggerGlitz,
-    mt::std::vector<RetardedNotActuallyAGlitzGlitz>::type const& foregroundTriggerGlitz,
-    mt::std::vector<Glitz>::type const& forwardsGlitz,
-    mt::std::vector<Glitz>::type const& reverseGlitz);
+
 template<typename RandomAccessGuyRange>
 bool currentPlayerInArrivals(RandomAccessGuyRange const& guyArrivals, std::size_t playerInputSize);
 enum {
