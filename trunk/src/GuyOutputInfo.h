67e9ada1ac66187a7d2449cfee85274c7dc2c837
evanwallace <evanwallace@5321f17f-e843-4a89-a37a-4f8a1ec584ce>
2012-11-21 02:57:21 +0000

    Added inventory display.

--- trunk/src/GuyOutputInfo.h
+++ trunk/src/GuyOutputInfo.h
@@ -2,14 +2,18 @@
 #define HG_GUY_OUTPUT_INFO_H
 #include <cstddef>
 #include "TimeDirection.h"
+#include "Ability.h"
+#include "mt/std/map"
 namespace hg {
 class GuyOutputInfo {
 public:
-    explicit GuyOutputInfo(TimeDirection timeDirection) :
-        timeDirection_(timeDirection) {}
+    explicit GuyOutputInfo(TimeDirection timeDirection, mt::std::map<Ability, int>::type const& pickups) :
+        timeDirection_(timeDirection), pickups_(pickups) {}
     TimeDirection getTimeDirection() const { return timeDirection_; }
+    mt::std::map<Ability, int>::type const& getPickups() const { return pickups_; }
 private:
     TimeDirection timeDirection_;
+    mt::std::map<Ability, int>::type pickups_;
 };
 }//namespace hg
 #endif //HG_GUY_OUTPUT_INFO_H
