bcc06e0782530ae0398f6a59b3f0f206c1ffa41b
Evan Wallace <onlyone@senet.com.au>
2013-01-04 16:17:18 +0000

    Changed Control Scheme: 1,2,3,4 select item WASD movement K save replay left-click - use item Right mouse button - select time target for item

--- src/Hg_Input.cpp
+++ src/Hg_Input.cpp
@@ -2,7 +2,7 @@
 #include "InputList.h"
 namespace hg
 {
-static void updatePress(int &var, bool inputState)
+static void updatePress(int& var, bool inputState)
 {
     if (inputState)
     {
@@ -22,60 +22,62 @@ static void updatePress(int &var, bool inputState)
 }
 
 Input::Input() :
-        left(false),
-        right(false),
-        up(false),
-        q(0),
-        w(0),
-        down(0),
-        space(0),
-        mouseLeft(0),
-        mouseRight(0),
-        mouseX(0),
-        mouseY(0),
-        timelineLength(0)
+    left(),
+    right(),
+    up(),
+    down(),
+    space(),
+    mouseLeft(),
+    abilityCursor(),
+    mouseTimelinePosition(),
+    mouseX(),
+    mouseY(),
+    timelineLength()
 {
 }
 
 void Input::updateState(const sf::Input& input, int mouseXOfEndOfTimeline, double mouseScale)
 {
-    left = input.IsKeyDown(sf::Key::Left);
-    right = input.IsKeyDown(sf::Key::Right);
-    up = input.IsKeyDown(sf::Key::Up);
-    updatePress(q, input.IsKeyDown(sf::Key::Q));
-    updatePress(w, input.IsKeyDown(sf::Key::W));
-    updatePress(down, input.IsKeyDown(sf::Key::Down));
+    left = input.IsKeyDown(sf::Key::A);
+    right = input.IsKeyDown(sf::Key::D);
+    up = input.IsKeyDown(sf::Key::W);
+    updatePress(down, input.IsKeyDown(sf::Key::S));
     updatePress(space, input.IsKeyDown(sf::Key::Space));
+    
     updatePress(mouseLeft, input.IsMouseButtonDown(sf::Mouse::Left));
-    updatePress(mouseRight, input.IsMouseButtonDown(sf::Mouse::Right));
+
+    if (input.IsKeyDown(sf::Key::Num1)) {
+        abilityCursor = TIME_JUMP;
+    }
+    if (input.IsKeyDown(sf::Key::Num2)) {
+        abilityCursor = TIME_REVERSE;
+    }
+    if (input.IsKeyDown(sf::Key::Num3)) {
+        abilityCursor = TIME_GUN;
+    }
+    if (input.IsKeyDown(sf::Key::Num4)) {
+        abilityCursor = TIME_PAUSE;
+    }
+    if (input.IsMouseButtonDown(sf::Mouse::Right)) {
+        mouseTimelinePosition = static_cast<int>(input.GetMouseX()*timelineLength/static_cast<double>(mouseXOfEndOfTimeline));
+    }
     mouseX = input.GetMouseX()*mouseScale;
     mouseY = input.GetMouseY()*mouseScale;
-    mouseTimelinePosition = static_cast<int>(input.GetMouseX() * timelineLength / static_cast<double>(mouseXOfEndOfTimeline));
 }
 
 InputList Input::AsInputList() const
 {
-    if (mouseLeft == 1)
-    {
-        return InputList(left, right, up, (down == 1), (space == 1), hg::TIME_JUMP, FrameID(mouseTimelinePosition, UniverseID(timelineLength)), 0, 0);
-    }
-
-    if (mouseRight == 1)
-    {
-        return InputList(
-            left, right, up, (down == 1), (space == 1),
-            hg::TIME_GUN, FrameID(mouseTimelinePosition, UniverseID(timelineLength)), mouseX, mouseY);
-    }
-
-    if (q == 1) {
-        return InputList(left, right, up, (down == 1), (space == 1), hg::TIME_PAUSE, FrameID(), 0, 0);
-    }
-
-    if (w == 1) {
-		return InputList(left, right, up, (down == 1), (space == 1), hg::TIME_REVERSE, FrameID(), 0, 0);
-    }
-
-    return InputList(left, right, up, (down == 1), (space == 1), hg::NO_ABILITY, FrameID(), 0, 0);
+    return InputList(
+        left,
+        right,
+        up,
+        down,
+        space == 1,     //portalUsed
+        mouseLeft == 1, //abilityUsed
+        abilityCursor,  
+        FrameID(mouseTimelinePosition, UniverseID(timelineLength)),     
+        mouseX,
+        mouseY);
 }
 }
 
--- src/ReplayIO_test.cpp
+++ src/ReplayIO_test.cpp
@@ -7,9 +7,9 @@ namespace {
     bool testReplaySavingAndLoading()
     {
         std::vector<InputList> inputReplay;
-        inputReplay.push_back(InputList(false, true, false, false, false, NO_ABILITY, FrameID(), 0, 0));
-        inputReplay.push_back(InputList(false, true, false, false, false, NO_ABILITY, FrameID(), 0, 0));
-        inputReplay.push_back(InputList(false, false, false, false, false, TIME_JUMP, FrameID(35, UniverseID(20000)), 0, 0));
+        inputReplay.push_back(InputList(false, true, false, false, false, false, NO_ABILITY, FrameID(), 0, 0));
+        inputReplay.push_back(InputList(false, true, false, false, false, false,NO_ABILITY, FrameID(), 0, 0));
+        inputReplay.push_back(InputList(false, false, false, false, false, true, TIME_JUMP, FrameID(35, UniverseID(20000)), 0, 0));
         std::stringstream stream;
         saveReplay(stream, inputReplay);
         return inputReplay == loadReplay(stream);
