53716ea2371f3634c505f5dd7a9c50d615384387
googlefrog <googlefrog@5321f17f-e843-4a89-a37a-4f8a1ec584ce>
2013-01-12 13:39:02 +0000

    Added multi-sticky switch. All buttons must be held down for switch to be enabled and become sticky. At some point I will clean up these switch classes.
    Added a reverse time level involving six switches and a box. I think it is possible in theory, will work on doing it later.

---
+++ trunk/levels/HoldThemAll/triggerSystem.lua
@@ -0,0 +1,116 @@
+local bts = require "global.basicTriggerSystem"
+
+local tempStore =
+{
+    protoPortals =
+    {
+        {
+            attachment = {platform = nil, xOffset = 18 * 3200, yOffset = 5 * 3200},
+            index = 1,
+            width = 2 * 3200,
+            height = 2 * 3200,
+            collisionOverlap = 50,
+            timeDirection = 'reverse',
+            destinationIndex = 1,
+            xDestination = 0,
+            yDestination = 0,
+            relativeTime = false,
+            timeDestination = 0,
+            illegalDestination = 1,
+            fallable = false,
+            winner = true
+        }
+    },
+    protoCollisions = {
+        {
+            width = 3200,
+            height = 4 * 3200,
+            timeDirection = 'forwards',
+            lastStateTriggerID = 2,
+            buttonTriggerID = 1,
+            destinations =
+            {
+                onDestination = {
+                    xDestination = {
+                        desiredPosition = 16 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 50,
+                        deceleration = 50
+                    },
+                    yDestination = {
+                        desiredPosition = -1 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    }
+                },
+                offDestination = {
+                    xDestination = {
+                        desiredPosition = 16 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 50,
+                        deceleration = 50
+                    },
+                    yDestination = {
+                        desiredPosition = 3 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    }
+                }
+            }
+        }
+    },
+    protoMutators = {
+    },
+    protoButtons = {
+        bts.multiStickySwitch{
+            buttons = {
+				{
+					attachment = {platform = nil, xOffset = 3.25 * 3200, yOffset = 6.75 * 3200},
+					width = 1600,
+					height = 800,
+				},
+				{
+					attachment = {platform = nil, xOffset = 5.25 * 3200, yOffset = 6.75 * 3200},
+					width = 1600,
+					height = 800,
+				},
+				{
+					attachment = {platform = nil, xOffset = 7.25 * 3200, yOffset = 6.75 * 3200},
+					width = 1600,
+					height = 800,
+				},
+				{
+					attachment = {platform = nil, xOffset = 9.25 * 3200, yOffset = 6.75 * 3200},
+					width = 1600,
+					height = 800,
+				},
+				{
+					attachment = {platform = nil, xOffset = 11.25 * 3200, yOffset = 6.75 * 3200},
+					width = 1600,
+					height = 800,
+				},
+				{
+					attachment = {platform = nil, xOffset = 13.25 * 3200, yOffset = 6.75 * 3200},
+					width = 1600,
+					height = 800,
+				},
+			},
+            timeDirection = 'forwards',
+            triggerID = 1
+        },
+    }
+}
+
+calculatePhysicsAffectingStuff = bts.calculatePhysicsAffectingStuff(tempStore)
+
+function shouldArrive(dynamicObject)
+    return true
+end
+function shouldPort(responsiblePortalIndex, dynamicObject, porterActionedPortal)
+    return true
+end
+mutateObject = bts.mutateObject(tempStore)
+
+getDepartureInformation = bts.getDepartureInformation(tempStore)
