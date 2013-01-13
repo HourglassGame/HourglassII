0d243f0d992f1fcb06e03b34b133542b49feed9b
GoogleFrog <googl3frog@gmail.com>
2013-01-13 05:46:56 +0000

    Reversed time directions in HoldThemAll such that triggers and platform works forwards. The replay does not need to be redone.

--- levels/HoldThemAll.lvl/main.lua
+++ levels/HoldThemAll.lvl/main.lua
@@ -21,7 +21,7 @@ environment =
 }
 initialGuy =
 {
-    arrivalTime = 3000,
+    arrivalTime = 4000,
     arrival =
     {
         x = 1.5 * 3200,
@@ -29,11 +29,11 @@ initialGuy =
         width = 1600,
         height = 3200,
         facing = "right",
-        timeDirection = "forwards",
+        timeDirection = "reverse",
         pickups = {timeJump = 0, timeReverse = 1, timeGun = 0, timePause = 0},
 		boxCarrying = true,
         boxCarrySize = 3200,
-        boxCarryDirection = "forwards",
+        boxCarryDirection = "reverse",
     }
 }
 initialArrivals =
@@ -49,11 +49,11 @@ triggerSystem =
     -- C++ still manages offsets and defaults.
     triggerOffsetsAndDefaults = {
         {
-            offset = -1,
+            offset = 1,
             default = {0}
         },
         {
-            offset = -1,
+            offset = 1,
             default = {16 * 3200, 3 * 3200, 0, 0}
         }
     },
