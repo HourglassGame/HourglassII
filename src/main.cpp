3b74655aa4af8a38bd0e48984b2826716a7f2c47
Evan Wallace <onlyone@senet.com.au>
2013-09-15 12:38:08 +0000

    Fixed index error and made timeline contents display over other timeline data.

--- src/main.cpp
+++ src/main.cpp
@@ -766,19 +766,14 @@ void DrawTimelineContents(
             
             double x = hg::clamp(0., 255.*guy.getX()/timeEngine.getWall().roomWidth(), 255.);
             double y = hg::clamp(0., 255.*guy.getY()/timeEngine.getWall().roomHeight(), 255.);
-            if (guy.getBoxCarrying())
-            {
-            	timelineContents.SetPixel(left, top, guy.getBoxCarryDirection() == guy.getTimeDirection() ? Colour(255, x, y) : Colour(0, x, y));
-            	timelineContents.SetPixel(left, top+1, guy.getBoxCarryDirection() == guy.getTimeDirection() ? Colour(255, x, y) : Colour(0, x, y));
-            	timelineContents.SetPixel(left, top+2, guy.getBoxCarryDirection() == guy.getTimeDirection() ? Colour(255, x, y) : Colour(0, x, y));
-            	timelineContents.SetPixel(left, top+3, guy.getBoxCarryDirection() == guy.getTimeDirection() ? Colour(255, x, y) : Colour(0, x, y));
-            }
-            else
-            {
-            	 timelineContents.SetPixel(left, top, Colour(128,x,y));
-            	 timelineContents.SetPixel(left, top+1, Colour(128,x,y));
-            	 timelineContents.SetPixel(left, top+2, Colour(128,x,y));
-            	 timelineContents.SetPixel(left, top+3, Colour(128,x,y));
+            for (int pos(top), end(std::min(height, top+4)); pos != end; ++pos) {
+                timelineContents.SetPixel(
+                    left, pos,
+                    !guy.getBoxCarrying() ?
+                        Colour(128,x,y) :
+                        guy.getBoxCarryDirection() == guy.getTimeDirection() ?
+                            Colour(255, x, y)
+                          : Colour(0, x, y));
             }
         }
     }
@@ -850,7 +845,6 @@ void DrawTimeline(
     int timelineLength)
 {
     double height = 75.;
-    DrawTimelineContents(target, timeEngine, height);
     
     DrawWaves(target, waves, timelineLength, height);
     
@@ -872,6 +866,7 @@ void DrawTimeline(
                 10.f+height,
                 Colour(0,0,200)));
     }
+    DrawTimelineContents(target, timeEngine, height);
 }
 
 struct CompareIndicies {
