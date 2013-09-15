970fda2ac52936999469141fb0a960820a478915
googlefrog <googlefrog@5321f17f-e843-4a89-a37a-4f8a1ec584ce>
2013-09-15 11:54:05 +0000

    Messed with the colour used in the timeline display and added a distinction between forwards and reverse boxes.

--- trunk/src/GuyOutputInfo.h
+++ trunk/src/GuyOutputInfo.h
@@ -7,12 +7,13 @@
 namespace hg {
 class GuyOutputInfo {
 public:
-    explicit GuyOutputInfo(std::size_t index, TimeDirection timeDirection, mt::std::map<Ability, int>::type const& pickups, bool boxCarrying, int x, int y) :
-        index(index), timeDirection(timeDirection), pickups(pickups), boxCarrying(boxCarrying), x(x), y(y) {}
+    explicit GuyOutputInfo(std::size_t index, TimeDirection timeDirection, mt::std::map<Ability, int>::type const& pickups, bool boxCarrying, TimeDirection boxCarryDirection, int x, int y) :
+        index(index), timeDirection(timeDirection), pickups(pickups), boxCarrying(boxCarrying), boxCarryDirection(boxCarryDirection), x(x), y(y) {}
     std::size_t getIndex() const { return index; }
-    bool getBoxCarrying() const { return boxCarrying; }
     TimeDirection getTimeDirection() const { return timeDirection; }
     mt::std::map<Ability, int>::type const& getPickups() const { return pickups; }
+    bool getBoxCarrying() const { return boxCarrying; }
+    TimeDirection getBoxCarryDirection() const { return boxCarryDirection; }
     int getX() const { return x; }
     int getY() const { return y; }
 private:
@@ -20,6 +21,7 @@ private:
     TimeDirection timeDirection;
     mt::std::map<Ability, int>::type pickups;
     bool boxCarrying;
+    TimeDirection boxCarryDirection;
     int x;
     int y;
 };
