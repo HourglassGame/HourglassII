f9f102547314640f03ecefb580eb219d6aaa6a79
Evan Wallace <onlyone@senet.com.au>
2013-09-17 02:38:23 +0000

    Make level.lvl be the example file again (I screwed up with the previous commits)

--- data/level.lvl/triggerSystem.lua
+++ data/level.lvl/triggerSystem.lua
@@ -1,54 +1,130 @@
 local bts = require "global.basicTriggerSystem"
 
-local tempStore =
+local tempStore = 
 {
-    --constant proto-object data:
     protoPortals =
     {
         {
-            attachment = {platform = nil, xOffset = 10 * 3200, yOffset = 2 * 3200},
-            index = 2,
-            width = 2 * 3200,
-            height = 2 * 3200,
+            attachment = {platform = 1, xOffset = -4200, yOffset = -3200},
+            index = 1,
+            width = 4200,
+            height = 4200,
             collisionOverlap = 50,
             timeDirection = 'forwards',
             destinationIndex = 1,
             xDestination = 0,
-            yDestination = 0,
-            relativeTime = false,
-            timeDestination = 0,
+            yDestination = -16000,
+            relativeTime = true,
+            timeDestination = 120,
             illegalDestination = 1,
-            fallable = false,
-            winner = true
+            fallable = true,
+            winner = false,
         }
     },
-    protoCollisions = {  
+    protoCollisions = {
+        {
+            width = 6400,
+            height = 1600,
+            timeDirection = 'forwards',
+            lastStateTriggerID = 2,
+            buttonTriggerID = 1,
+            destinations =
+            {
+                onDestination = {
+                    xDestination = {
+                        desiredPosition = 22400,
+                        maxSpeed = 200,
+                        acceleration = 50,
+                        deceleration = 50
+                    },
+                    yDestination = {
+                        desiredPosition = 43800,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    }
+                },
+                offDestination = {
+                    xDestination = {
+                        desiredPosition = 38400,
+                        maxSpeed = 200,
+                        acceleration = 50,
+                        deceleration = 50
+                    },
+                    yDestination = {
+                        desiredPosition = 43800,
+                        maxSpeed = 300,
+                        acceleration = 20,
+                        deceleration = 20
+                    }
+                }
+            }
+        }
     },
-    protoMutators = {
-        bts.pickup{
-            attachment = {platform = nil, xOffset = 1.25 * 3200, yOffset = 6.25 * 3200},
-            width = 1600,
-            height = 1600,
-            timeDirection = 'forwards',
-            triggerID = 1,
-			pickupType = 'timeJump'
-        },
-		bts.pickup{
-            attachment = {platform = nil, xOffset = 12.25 * 3200, yOffset = 3.25 * 3200},
-            width = 1600,
-            height = 1600,
-            timeDirection = 'forwards',
-            triggerID = 2,
-			pickupType = 'timeJump'
-        },
+    protoMutators = {},
+    protoMuts = {
+        {
+            data = {
+                x = 50000,
+                y = 25600,
+                xspeed = 0,
+                yspeed = 0,
+                width = 800,
+                height = 6400,
+                collisionOverlap = 0,
+                timeDirection = "forwards",
+            },
+            effect = function (self, object) -- butterfingers
+                if object.type == "guy" and object.boxCarrying then
+                    object.boxCarrying = false
+                    self.additionalEndBoxes[#self.additionalEndBoxes+1] = {
+                        box = {
+                            x = object.x + object.width/2 - object.boxCarrySize/2, 
+                            y = object.y - object.boxCarrySize, 
+                            xspeed = -object.xspeed, yspeed = -500, 
+                            size = object.boxCarrySize, 
+                            illegalPortal = nil, 
+                            arrivalBasis = nil, 
+                            timeDirection = object.boxCarryDirection}, 
+                        targetFrame = self.frameNumber+bts.timeDirectionToInt(object.boxCarryDirection),
+                    }
+                end
+                return object
+            end,
+        }
     },
     protoButtons = {
+        bts.momentarySwitch{
+            attachment = {platform = 1, xOffset = 3200, yOffset = -800},
+            width = 3200,
+            height = 800,
+            timeDirection = 'forwards',
+            triggerID = 1
+        },
+        bts.momentarySwitch{
+            attachment = {platform = nil, xOffset = 3200, yOffset = 37600},
+            width = 3200,
+            height = 800,
+            timeDirection = 'forwards',
+            triggerID = 3
+        }
     }
 }
+local function calculateMutatorGlitz(protoMutator)
+    return bts.calculateBidirectionalGlitz(450, protoMutator, {r = 150, g = 150, b = 150}, {r = 150, g = 150, b = 150})
+end
 --==Callin Definitions==--
 --triggerArrivals have already had default values inserted by C++
 --for trigger indices that did not arrive by the time this is called
-calculatePhysicsAffectingStuff = bts.calculatePhysicsAffectingStuff(tempStore)
+function calculatePhysicsAffectingStuff(frameNumber, triggerArrivals)
+    local retv = bts.calculatePhysicsAffectingStuff(tempStore)(frameNumber, triggerArrivals)
+    
+    tempStore.makeBox = (frameNumber == 2000 and triggerArrivals[3][1] == 0)
+    retv.mutators = { [1] = tempStore.protoMuts[1].data}
+    tempStore.portalActive = (triggerArrivals[3][1] == 1)
+    
+    return retv
+end
 
 --responsible*Index gives the position in the list of the thing that
 --is responsible for the callin happening.
@@ -59,12 +135,27 @@ calculatePhysicsAffectingStuff = bts.calculatePhysicsAffectingStuff(tempStore)
 function shouldArrive(dynamicObject)
     return true
 end
-function shouldPort(responsiblePortalIndex, dynamicObject, porterActionedPortal)
-    return true
+function shouldPort(responsiblePortalIndex, dynamicObject, porterActionedPortal) 
+    return tempStore.portalActive
 end
 function mutateObject(responsibleManipulatorIndices, dynamicObject)
-    return dynamicObject
+    return tempStore.protoMuts[1].effect(tempStore, dynamicObject)
 end
-mutateObject = bts.mutateObject(tempStore)
-getDepartureInformation = bts.getDepartureInformation(tempStore)
 
+function getDepartureInformation(departures)
+    local outputTriggers, forwardsGlitz, reverseGlitz, additionalEndBoxes = bts.getDepartureInformation(tempStore)(departures)
+    
+    local forwardsMutGlitz, reverseMutGlitz = calculateMutatorGlitz(tempStore.protoMuts[1].data)
+    table.insert(forwardsGlitz, forwardsMutGlitz)
+    table.insert(reverseGlitz, reverseMutGlitz)
+
+    if tempStore.makeBox then
+        additionalEndBoxes[#additionalEndBoxes+1] = {
+            box = {x = 12800, y = 6400, xspeed = -600, yspeed = -400, size = 3200,
+                   illegalPortal = nil, arrivalBasis = nil, timeDirection = 'forwards'}, 
+            targetFrame = 500
+        }
+    end
+    
+    return outputTriggers, forwardsGlitz, reverseGlitz, additionalEndBoxes
+end
