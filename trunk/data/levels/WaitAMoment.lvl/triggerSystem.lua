78b6ae874016929f3d1070f73f6612c53a770e42
googlefrog <googlefrog@5321f17f-e843-4a89-a37a-4f8a1ec584ce>
2013-09-20 18:00:47 +0000

    Added level "Wait A Moment"

---
+++ trunk/data/levels/WaitAMoment.lvl/triggerSystem.lua
@@ -0,0 +1,67 @@
+local bts = require "global.basicTriggerSystem"
+
+local tempStore =
+{
+    protoPortals =
+    {
+        {
+            attachment = {platform = nil, xOffset = 6.5 * 3200, yOffset = 4 * 3200},
+            index = 1,
+            width = 2 * 3200,
+            height = 2 * 3200,
+            collisionOverlap = 50,
+            timeDirection = 'forwards',
+            destinationIndex = 1,
+            xDestination = 0,
+            yDestination = 0,
+            relativeTime = false,
+            timeDestination = 0,
+            illegalDestination = 1,
+            fallable = false,
+            winner = false
+        },
+        {
+            attachment = {platform = nil, xOffset = 2 * 3200, yOffset = 2 * 3200},
+            index = 2,
+            width = 2 * 3200,
+            height = 2 * 3200,
+            collisionOverlap = 50,
+            timeDirection = 'forwards',
+            destinationIndex = 2,
+            xDestination = 0,
+            yDestination = 0,
+            relativeTime = false,
+            timeDestination = 0,
+            illegalDestination = 2,
+            fallable = false,
+            winner = true
+        }
+    },
+    protoCollisions = {
+    },
+    protoMutators = {
+        bts.pickup{
+            attachment = {platform = nil, xOffset = 9.25 * 3200, yOffset = 5.25 * 3200},
+            width = 1600,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 1,
+			pickupType = 'timePause',
+			pickupNumber = 2
+        },
+    },
+    protoButtons = {
+    }
+}
+
+calculatePhysicsAffectingStuff = bts.calculatePhysicsAffectingStuff(tempStore)
+
+function shouldArrive(dynamicObject)
+    return true
+end
+function shouldPort(responsiblePortalIndex, dynamicObject, porterActionedPortal)
+    return true
+end
+mutateObject = bts.mutateObject(tempStore)
+
+getDepartureInformation = bts.getDepartureInformation(tempStore)
