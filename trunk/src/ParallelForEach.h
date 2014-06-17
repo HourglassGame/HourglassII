b29f08bc2774c6a0cb336733c07b030e549f401d
evanwallace <evanwallace@5321f17f-e843-4a89-a37a-4f8a1ec584ce>
2014-06-17 10:06:19 +0000

    Added soundsGlitz for momentarySwitch, stickySwitch, and toggleSwitch. Still todo: multiStickySwitch.

--- trunk/src/ParallelForEach.h
+++ trunk/src/ParallelForEach.h
@@ -3,21 +3,15 @@
 #include <boost/range.hpp>
 #include <tbb/parallel_do.h>
 #include <boost/range/algorithm/for_each.hpp>
+#include "as_lvalue.h"
+
 namespace hg {
 template<typename RandomAccessRange, typename Func>
 void parallel_for_each(
-	RandomAccessRange &range, Func func, tbb::task_group_context &context)
+	RandomAccessRange &range, Func &&func, tbb::task_group_context &context = as_lvalue(tbb::task_group_context()))
 {
-    tbb::parallel_do(boost::begin(range), boost::end(range), func, context);
+    tbb::parallel_do(boost::begin(range), boost::end(range), std::forward<Func>(func), context);
     //boost::for_each(range, func);
-}
-
-template<typename RandomAccessRange, typename Func>
-void parallel_for_each(
-	RandomAccessRange &range, Func func)
-{
-    tbb::task_group_context context;
-    parallel_for_each(range, func, context);
 }
 }
 #endif //HG_PARALLEL_FOR_EACH_H
