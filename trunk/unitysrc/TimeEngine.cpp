bab1b3dfcfcef230c6c5b3f82be6630ffbe1047a
evanwallace <evanwallace@5321f17f-e843-4a89-a37a-4f8a1ec584ce>
2014-07-19 14:08:13 +0000

    Added 8AllTheWayUp, Added comment explaining illegal portal, removed some completed items from the todo list, added files to support a 'unity' build

---
+++ trunk/unitysrc/TimeEngine.cpp
@@ -0,0 +1,23 @@
+//Timeline Management
+#include "../src/ConcurrentFrameUpdateSet.cpp"
+#include "../src/ConcurrentTimeMap.cpp"
+#include "../src/ConcurrentTimeSet.cpp"
+#include "../src/DepartureMap.cpp"
+#include "../src/TimeDirection.cpp"
+#include "../src/TimeEngine.cpp"
+#include "../src/WorldState.cpp"
+#include "../src/TimelineState.cpp"
+
+//ObjectList
+#include "../src/Guy.cpp"
+#include "../src/Box.cpp"
+#include "../src/ObjectList_inst_NonGuyDynamic.cpp"
+#include "../src/ObjectList_inst_Normal.cpp"
+#include "../src/ObjectPtrList_inst_Normal.cpp"
+#include "../src/GlitzPersister.cpp"
+
+//Frame
+#include "../src/UniverseID.cpp"
+#include "../src/FrameID.cpp"
+#include "../src/Universe.cpp"
+#include "../src/Frame.cpp"
