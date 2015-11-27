6ad724a452c8f77fc63bbb18eff32fef5be8d715
GoogleFrog <googl3frog@gmail.com>
2015-11-28 01:31:39 +1100

    Added level Opposites Annihilate.

---
+++ data/levels/OppositesAnnihilate.lvl/main.lua
@@ -0,0 +1,59 @@
+name = "Opposites Annihilate"
+speedOfTime = 3
+timelineLength = 5401 -- Important, improves loading time.
+environment =
+{
+    gravity = 22,
+    wall =
+    {
+        width = 18,
+        height = 11,
+        segmentSize = 3200,
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1},
+        {1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1},
+        {1,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1},
+        {1,0,0,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+    }
+}
+initialGuy =
+{
+    arrivalTime = 0,
+    arrival =
+    {
+        x = 10.5 * 3200,
+        y = 8 * 3200,
+        width = 1600,
+        height = 3200,
+        facing = "left",
+        timeDirection = "forwards",
+        pickups = {timeJump = 0, timeReverse = 0, timeGun = 0},
+    }
+}
+initialArrivals =
+{
+	{type = "box", x = 4.5 * 3200, y = 6 * 3200, xspeed = 0, yspeed = 0, size = 3200, timeDirection = "forwards"},
+	{type = "box", x = 4.5 * 3200, y = 6 * 3200, xspeed = 0, yspeed = 0, size = 3200, timeDirection = "reverse"},
+}
+triggerSystem =
+{
+    type = "DirectLua",
+
+    luaFiles = {"triggerSystem", "global.basicTriggerSystem"},
+    system = [==[require "triggerSystem"]==],
+
+    -- C++ still manages offsets and defaults.
+    triggerOffsetsAndDefaults = {
+        {
+            offset = 1,
+            default = {11 * 3200, 11 * 3200, 0, 0}
+        },
+    },
+    arrivalLocationsSize = 3,
+}
---
+++ data/levels/OppositesAnnihilate.lvl/triggerSystem.lua
@@ -0,0 +1,44 @@
+local bts = require "global.basicTriggerSystem"
+
+local tempStore =
+{
+    protoPortals =
+    {
+        {
+            attachment = {xOffset = 14.5 * 3200, yOffset = 4 * 3200},
+            index = 2,
+            width = 2 * 3200,
+            height = 2 * 3200,
+            collisionOverlap = 50,
+            timeDirection = 'forwards',
+            destinationIndex = 2,
+            xDestination = 0,
+            yDestination = 0,
+            relativeTime = false,
+            timeDestination = 0,
+            illegalDestination = 1,
+            fallable = false,
+            winner = true
+        },
+    },
+    protoCollisions = {
+    },
+	protoMutators = {
+	},
+    protoButtons = {
+    },
+	protoGlitz = {
+	},
+}
+
+calculatePhysicsAffectingStuff = bts.calculatePhysicsAffectingStuff(tempStore)
+
+function shouldArrive(dynamicObject)
+    return true
+end
+function shouldPort(responsiblePortalIndex, dynamicObject, porterActionedPortal)
+    return true
+end
+mutateObject = bts.mutateObject(tempStore)
+
+getDepartureInformation = bts.getDepartureInformation(tempStore)
