e1235d34dd4ef40429e942ffc5dd8e54729e7dad
GoogleFrog <googl3frog@gmail.com>
2015-12-01 19:56:26 +1100

    Added level 31, Ups and Downs. Cliffs are shortened for jump height change. Solution is different. I think the HG 1 version relies on no paradox checking in the future.

---
+++ data/levels/31UpsAndDowns.lvl/triggerSystem.lua
@@ -0,0 +1,62 @@
+local bts = require "global.basicTriggerSystem"
+
+local tempStore =
+{
+    protoPortals =
+    {
+		{
+            attachment = {xOffset = 24.25 * 3200, yOffset = 4 * 3200},
+            index = 1,
+            width = 2 * 3200,
+            height = 2 * 3200,
+            collisionOverlap = 50,
+            timeDirection = 'forwards',
+            destinationIndex = 1,
+            xDestination = 0,
+            yDestination = 0,
+            relativeTime = false,
+            timeDestination = 0,
+            illegalDestination = 1,
+            fallable = false,
+            winner = true
+        },
+        {
+            attachment = {xOffset = 1.75 * 3200, yOffset = 10 * 3200},
+            index = 2,
+            width = 2 * 3200,
+            height = 2 * 3200,
+            collisionOverlap = 50,
+            timeDirection = 'forwards',
+            destinationIndex = 2,
+            xDestination = 0,
+            yDestination = 0,
+            relativeTime = false,
+            timeDestination = 0,
+            illegalDestination = 1,
+            fallable = false,
+            winner = false
+        },
+    },
+    protoCollisions = {
+    },
+	protoMutators = {
+        
+	},
+    protoButtons = {
+		
+    },
+	protoGlitz = {
+	},
+}
+
+calculatePhysicsAffectingStuff = bts.calculatePhysicsAffectingStuff(tempStore)
+
+function shouldArrive(dynamicObject)
+    return true
+end
+function shouldPort(responsiblePortalIndex, dynamicObject, porterActionedPortal)
+    return true
+end
+mutateObject = bts.mutateObject(tempStore)
+
+getDepartureInformation = bts.getDepartureInformation(tempStore)
