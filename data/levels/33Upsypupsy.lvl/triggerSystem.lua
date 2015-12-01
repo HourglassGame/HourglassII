3a1bf8ac7924eeeba329e1538ef55c2aacf82454
GoogleFrog <googl3frog@gmail.com>
2015-12-01 21:09:08 +1100

    Added level 33 Upsypupsy. The level is possible without modifying the terrain for the jump height change. My solution is different (it involves the use of reverse boxes). This level is amazing.

---
+++ data/levels/33Upsypupsy.lvl/triggerSystem.lua
@@ -0,0 +1,121 @@
+local bts = require "global.basicTriggerSystem"
+
+local tempStore =
+{
+    protoPortals =
+    {
+        {
+            attachment = {xOffset = 1.5 * 3200, yOffset = 5 * 3200},
+            index = 2,
+            width = 2 * 3200,
+            height = 2 * 3200,
+            collisionOverlap = 50,
+            timeDirection = 'forwards',
+            destinationIndex = 2,
+            xDestination = 0,
+            yDestination = 0,
+            relativeTime = false,
+            timeDestination = 0,
+            illegalDestination = 1,
+            fallable = false,
+            winner = true
+        },
+    },
+    protoCollisions = {
+        {
+            width = 1 * 3200,
+            height = 2 * 3200,
+            timeDirection = 'forwards',
+            lastStateTriggerID = 2,
+            buttonTriggerID = 1,
+            destinations =
+            {
+                onDestination = {
+                    xDestination = {
+                        desiredPosition = 4 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 20,
+                        deceleration = 20
+                    },
+                    yDestination = {
+                        desiredPosition = 3 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 20,
+                        deceleration = 20
+                    }
+                },
+                offDestination = {
+                    xDestination = {
+                        desiredPosition = 4 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 20,
+                        deceleration = 20
+                    },
+                    yDestination = {
+                        desiredPosition = 5 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 20,
+                        deceleration = 20
+                    }
+                }
+            }
+        },
+    },
+	protoMutators = {
+        bts.pickup{
+            attachment = {platform = nil, xOffset = 8 * 3200, yOffset = 6.25 * 3200},
+            width = 1600,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 3,
+            pickupType = 'timeReverse'
+        },
+        bts.pickup{
+            attachment = {platform = nil, xOffset = 13.5 * 3200, yOffset = 11.25 * 3200},
+            width = 1600,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 4,
+            pickupType = 'timeReverse'
+        },
+        bts.pickup{
+            attachment = {platform = nil, xOffset = 17 * 3200, yOffset = 8.25 * 3200},
+            width = 1600,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 5,
+            pickupType = 'timeReverse'
+        },
+        bts.pickup{
+            attachment = {platform = nil, xOffset = 22.5 * 3200, yOffset = 13.25 * 3200},
+            width = 1600,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 6,
+            pickupType = 'timeReverse'
+        },
+	},
+    protoButtons = {
+		bts.momentarySwitch{
+            attachment = {xOffset = 27.5 * 3200, yOffset = 10.75 * 3200},
+            width = 3200,
+            height = 800,
+            timeDirection = 'forwards',
+            triggerID = 1,
+        },
+    },
+	protoGlitz = {
+	},
+}
+
+calculatePhysicsAffectingStuff = bts.calculatePhysicsAffectingStuff(tempStore)
+
+function shouldArrive(dynamicObject)
+    return true
+end
+function shouldPort(responsiblePortalIndex, dynamicObject, porterActionedPortal)
+    return true
+end
+mutateObject = bts.mutateObject(tempStore)
+
+getDepartureInformation = bts.getDepartureInformation(tempStore)
