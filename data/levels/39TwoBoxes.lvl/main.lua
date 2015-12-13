f20ae72a88ba83f373595694635076ec2918351d
GoogleFrog <googl3frog@gmail.com>
2015-12-14 00:22:41 +1100

    Added level 39, Two Boxes.

---
+++ data/levels/39TwoBoxes.lvl/main.lua
@@ -0,0 +1,75 @@
+name = "Two Boxes?"
+speedOfTime = 3
+timelineLength = 7200
+environment =
+{
+    gravity = 22,
+    wall =
+    {
+        width = 32,
+        height = 11,
+        segmentSize = 3200,
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,1,1,0,0,0,1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1},
+        {1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
+        {1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,1,1,0,0,0,1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    }
+}
+initialGuy =
+{
+    arrivalTime = 0,
+    arrival =
+    {
+        x = 17.5 * 3200,
+        y = 6 * 3200,
+        width = 1600,
+        height = 3200,
+        facing = "right",
+        timeDirection = "forwards",
+        pickups = {timeJump = 0, timeReverse = 0, timeGun = 0},
+    }
+}
+initialArrivals =
+{
+	{type = "box", x = 26.5 * 3200, y = 3 * 3200, xspeed = 0, yspeed = 0, size = 3200, timeDirection = "forwards"},
+	{type = "box", x = 28.5 * 3200, y = 8 * 3200, xspeed = 0, yspeed = 0, size = 3200, timeDirection = "forwards"},
+}
+triggerSystem =
+{
+    type = "DirectLua",
+
+    luaFiles = {"triggerSystem", "global.basicTriggerSystem"},
+    system = [==[require "triggerSystem"]==],
+
+    -- C++ still manages offsets and defaults.
+    triggerOffsetsAndDefaults = {
+        {
+            offset = 1,
+            default = {0}
+        },
+        {
+            offset = 1,
+            default = {0}
+        },
+        {
+            offset = 1,
+            default = {7.5 * 3200, 4 * 3200, 0, 0}
+        },
+        {
+            offset = 1,
+            default = {0 * 3200, 0 * 3200, 0, 0}
+        },
+        {
+            offset = 1,
+            default = {1}
+        },
+    },
+    arrivalLocationsSize = 1,
+}
---
+++ data/levels/39TwoBoxes.lvl/triggerSystem.lua
@@ -0,0 +1,148 @@
+local bts = require "global.basicTriggerSystem"
+
+local tempStore =
+{
+    protoPortals =
+    {
+        {
+            attachment = {xOffset = 3.5 * 3200, yOffset = 4.5 * 3200},
+            index = 2,
+            width = 2 * 3200,
+            height = 2 * 3200,
+            collisionOverlap = 50,
+            timeDirection = 'forwards',
+            destinationIndex = 2,
+            xDestination = 0,
+            yDestination = 0,
+            relativeTime = false,
+            timeDestination = 0,
+            illegalDestination = 1,
+            fallable = false,
+            winner = true
+        },
+    },
+    protoCollisions = {
+        {
+            width = 1 * 3200,
+            height = 3 * 3200,
+            timeDirection = 'forwards',
+            lastStateTriggerID = 3,
+            buttonTriggerID = 1,
+            destinations =
+            {
+                onDestination = {
+                    xDestination = {
+                        desiredPosition = 7.5 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 20,
+                        deceleration = 20
+                    },
+                    yDestination = {
+                        desiredPosition = 7 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 20,
+                        deceleration = 20
+                    }
+                },
+                offDestination = {
+                    xDestination = {
+                        desiredPosition = 7.5 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 20,
+                        deceleration = 20
+                    },
+                    yDestination = {
+                        desiredPosition = 4 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 20,
+                        deceleration = 20
+                    }
+                }
+            }
+        },
+		{
+            width = 0 * 3200,
+            height = 0 * 3200,
+            timeDirection = 'forwards',
+            lastStateTriggerID = 4,
+            buttonTriggerID = 2,
+            destinations =
+            {
+                onDestination = {
+                    xDestination = {
+                        desiredPosition = 0 * 3200,
+                        maxSpeed = 120,
+                        acceleration = 20,
+                        deceleration = 20
+                    },
+                    yDestination = {
+                        desiredPosition = -1 * 3200,
+                        maxSpeed = 120,
+                        acceleration = 20,
+                        deceleration = 20
+                    }
+                },
+                offDestination = {
+                    xDestination = {
+                        desiredPosition = 0 * 3200,
+                        maxSpeed = 120,
+                        acceleration = 20,
+                        deceleration = 20
+                    },
+                    yDestination = {
+                        desiredPosition = 0 * 3200,
+                        maxSpeed = 120,
+                        acceleration = 20,
+                        deceleration = 20
+                    }
+                }
+            }
+        },
+    },
+	protoMutators = {
+        bts.pickup{
+            attachment = {platform = nil, xOffset = 18.75 * 3200, yOffset = 6 * 3200},
+            width = 1600,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 5,
+            pickupType = 'timeJump'
+        },
+        bts.spikes{
+            attachment = {platform = 2, xOffset = 9 * 3200, yOffset = 9.5 * 3200},
+            width = 8 * 3200,
+            height = 0.5 * 3200,
+            timeDirection = 'forwards',
+        },
+	},
+    protoButtons = {
+		bts.momentarySwitch{
+            attachment = {xOffset = 30.75 * 3200, yOffset = 2.5 * 3200},
+            width = 800,
+            height = 3200,
+            timeDirection = 'forwards',
+            triggerID = 1,
+        },
+        bts.stickyLaserSwitch{
+            triggerID = 2,
+            timeDirection = 'forwards',
+            attachment = {platform = nil, xOffset = 26.75 * 3200, yOffset = 7 * 3200 },
+            beamLength = 2*3200,
+			beamDirection = 1,
+        },
+    },
+	protoGlitz = {
+	},
+}
+
+calculatePhysicsAffectingStuff = bts.calculatePhysicsAffectingStuff(tempStore)
+
+function shouldArrive(dynamicObject)
+    return true
+end
+function shouldPort(responsiblePortalIndex, dynamicObject, porterActionedPortal)
+    return true
+end
+mutateObject = bts.mutateObject(tempStore)
+
+getDepartureInformation = bts.getDepartureInformation(tempStore)
