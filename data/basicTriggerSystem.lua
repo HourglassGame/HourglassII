HourglassGame/HourglassII

commit 76864b139396cc380a3b2b88020bf7d6464216eb

Added Portls which have the shape of a Laser. Button triggers now have state = 1 when pressed and start = 2 afterwards.

data/basicTriggerSystem.lua
@@ -97,7 +97,7 @@ local function calculateCollisions(protoCollisions, triggerArrivals, outputTrigg
         end
         
         
-        local active = (self.buttonTriggerID and triggerArrivals[self.buttonTriggerID][1] == 1) 
+        local active = (self.buttonTriggerID and triggerArrivals[self.buttonTriggerID][1] > 0) 
                     or (self.triggerFunction and self.triggerFunction(triggerArrivals, frameNumber))
         local destination = active and self.destinations.onDestination or self.destinations.offDestination
 
@@ -152,8 +152,8 @@ local function snapAttachment(objectTimeDirection, attachment, collisions)
     return x, y, xspeed, yspeed
 end
 
-local function calculatePortals(forwardsGlitz, reverseGlitz, protoPortals, collisions, triggerArrivals, frameNumber)
-    local function calculatePortal(protoPortal, collisions)
+local function calculatePortals(forwardsGlitz, reverseGlitz, protoPortals, protoLasers, collisions, triggerArrivals, frameNumber)
+    local function calculatePortal(protoPortal, collisions, isLaser)
         local x, y, xspeed, yspeed =
             snapAttachment(protoPortal.timeDirection, protoPortal.attachment, collisions)
         
@@ -161,6 +161,8 @@ local function calculatePortals(forwardsGlitz, reverseGlitz, protoPortals, colli
             index = protoPortal.index,
             x = x,
             y = y,
+			xaim = protoPortal.xaim,
+			yaim = protoPortal.yaim,
             width = protoPortal.width,
             height = protoPortal.height,
             xspeed = xspeed,
@@ -176,10 +178,12 @@ local function calculatePortals(forwardsGlitz, reverseGlitz, protoPortals, colli
             destinationDirection = protoPortal.destinationDirection or 'forwards',
             illegalDestination = protoPortal.illegalDestination,
             fallable = protoPortal.fallable,
+			isLaser = isLaser,
             winner = false
         }
         
         if protoPortal.winner then
+			-- Not necessary
             retPortal.winner = true
             retPortal.relativeTime = false
             retPortal.timeDestination = 1
@@ -208,7 +212,7 @@ local function calculatePortals(forwardsGlitz, reverseGlitz, protoPortals, colli
         
         -- Text does not support changing time directions
         local text
-        if portal.winner or (portal.timeDestination == 1 and portal.destinationDirection == 'reverse') then -- Hax upon hax
+        if portal.winner then
             text = active and "Win" or "Inactive"
         elseif portal.relativeTime and portal.timeDestination > 0 then
             text = "+" .. formatTime(portal.timeDestination)
@@ -248,15 +252,26 @@ local function calculatePortals(forwardsGlitz, reverseGlitz, protoPortals, colli
     local arrivalLocations = {}
 
     for i, protoPortal in ipairs(protoPortals) do
-        local portal, active = calculatePortal(protoPortal, collisions)
+        local portal, active = calculatePortal(protoPortal, collisions, false)
         calculatePortalGlitz(portal, forwardsGlitz, reverseGlitz, active)
-        arrivalLocations[i] = calculateArrivalLocation(portal)
+        arrivalLocations[#arrivalLocations + 1] = calculateArrivalLocation(portal)
         if active then
             portalCount = portalCount + 1
             portals[portalCount] = portal
         end
     end
-
+	
+	if protoLasers then
+		for i, protoLaser in ipairs(protoLasers) do
+			local portal, active = calculatePortal(protoLaser, collisions, true)
+			arrivalLocations[#arrivalLocations + 1] = calculateArrivalLocation(portal)
+			if active then
+				portalCount = portalCount + 1
+				portals[portalCount] = portal
+			end
+		end
+	end
+	
     return portals, arrivalLocations
 end
 
@@ -406,12 +421,16 @@ local function momentarySwitch(p)
             PnV = calculateButtonPositionAndVelocity(proto, collisions)
         end,
         updateState = function(self, departures, triggerArrivals)
-            state = checkPressed(constructDynamicArea(proto, PnV), departures)
-            justPressed = triggerArrivals[stateTriggerID][1] == 0 and state == true
-            justReleased = triggerArrivals[stateTriggerID][1] == 1 and state == false
+			local oldState = triggerArrivals[stateTriggerID][1]
+            state = (checkPressed(constructDynamicArea(proto, PnV), departures) and oldState + 1) or 0
+			if state > 2 then
+				state = 2
+			end
+            justPressed = oldState == 0 and state > 0
+            justReleased = oldState > 0 and state == 0
         end,
         calculateGlitz = function(self, forwardsGlitz, reverseGlitz, persistentGlitz)
-            local forGlitz, revGlitz = calculateButtonGlitz(proto, PnV, state)
+            local forGlitz, revGlitz = calculateButtonGlitz(proto, PnV, state > 0)
             table.insert(forwardsGlitz, forGlitz)
             table.insert(reverseGlitz, revGlitz)
             if justPressed then
@@ -426,12 +445,12 @@ local function momentarySwitch(p)
             end
         end,
         fillTrigger = function(self, outputTriggers)
-            outputTriggers[triggerID] = {state and 1 or 0}
-            outputTriggers[stateTriggerID] = {state and 1 or 0}
+            outputTriggers[triggerID] = {state}
+            outputTriggers[stateTriggerID] = {state}
             if p.extraTriggerIDs then
                 local extraTriggerIDs = p.extraTriggerIDs
                 for i = 1, #extraTriggerIDs do
-                    outputTriggers[extraTriggerIDs[i]] = {state and 1 or 0}
+                    outputTriggers[extraTriggerIDs[i]] = {state}
                 end
             end
         end,
@@ -458,11 +477,15 @@ local function stickySwitch(p)
             PnV = calculateButtonPositionAndVelocity(proto, collisions)
         end,
         updateState = function(self, departures, triggerArrivals)
-            state = triggerArrivals[stateTriggerID][1] == 1 or checkPressed(constructDynamicArea(proto, PnV), departures)
-            justPressed = triggerArrivals[stateTriggerID][1] == 0 and state
+			local oldState = triggerArrivals[stateTriggerID][1]
+            state = (oldState > 0 and oldState + 1) or (checkPressed(constructDynamicArea(proto, PnV), departures) and 1) or 0
+			if state > 2 then
+				state = 2
+			end
+            justPressed = oldState == 0 and state > 0
         end,
         calculateGlitz = function(self, forwardsGlitz, reverseGlitz, persistentGlitz)
-            local forGlitz, revGlitz = calculateButtonGlitz(proto, PnV, state)
+            local forGlitz, revGlitz = calculateButtonGlitz(proto, PnV, state > 0)
             table.insert(forwardsGlitz, forGlitz)
             table.insert(reverseGlitz, revGlitz)
             if justPressed then
@@ -472,12 +495,12 @@ local function stickySwitch(p)
             end
         end,
         fillTrigger = function(self, outputTriggers)
-            outputTriggers[triggerID] = {state and 1 or 0}
-            outputTriggers[stateTriggerID] = {state and 1 or 0}
+            outputTriggers[triggerID] = {state}
+            outputTriggers[stateTriggerID] = {state}
             if p.extraTriggerIDs then
                 local extraTriggerIDs = p.extraTriggerIDs
                 for i = 1, #extraTriggerIDs do
-                    outputTriggers[extraTriggerIDs[i]] = {state and 1 or 0}
+                    outputTriggers[extraTriggerIDs[i]] = {state}
                 end
             end
         end,
@@ -582,17 +605,21 @@ local function stickyLaserSwitch(p)
 			emitterPnV = calculateButtonPositionAndVelocity(protoEmitter, collisions)
         end,
         updateState = function(self, departures, triggerArrivals)
-            state = triggerArrivals[stateTriggerID][1] == 1 or checkPressed(constructDynamicArea(protoBeam, beamPnV), departures)
-            justPressed = triggerArrivals[stateTriggerID][1] == 0 and state
+			local oldState = triggerArrivals[stateTriggerID][1]
+            state = (oldState and oldState + 1) or (checkPressed(constructDynamicArea(proto, PnV), departures) and 1) or 0
+			if state > 2 then
+				state = 2
+			end
+            justPressed = oldState == 0 and state > 0
         end,
         calculateGlitz = function(self, forwardsGlitz, reverseGlitz, persistentGlitz)
-            local forGlitzBeam, revGlitzBeam = calculateBeamGlitz(protoBeam, beamPnV, state)
+            local forGlitzBeam, revGlitzBeam = calculateBeamGlitz(protoBeam, beamPnV, state > 0)
             if forGlitzBeam then
 				table.insert(forwardsGlitz, forGlitzBeam)
 				table.insert(reverseGlitz, revGlitzBeam)
 			end
 			
-			local forGlitz, revGlitz = calculateButtonGlitz(protoEmitter, emitterPnV, state)
+			local forGlitz, revGlitz = calculateButtonGlitz(protoEmitter, emitterPnV, state > 0)
             table.insert(forwardsGlitz, forGlitz)
             table.insert(reverseGlitz, revGlitz)
             if justPressed then
@@ -602,12 +629,12 @@ local function stickyLaserSwitch(p)
             end
         end,
         fillTrigger = function(self, outputTriggers)
-            outputTriggers[triggerID] = {state and 1 or 0}
-            outputTriggers[stateTriggerID] = {state and 1 or 0}
+            outputTriggers[triggerID] = {state}
+            outputTriggers[stateTriggerID] = {state}
             if p.extraTriggerIDs then
                 local extraTriggerIDs = p.extraTriggerIDs
                 for i = 1, #extraTriggerIDs do
-                    outputTriggers[extraTriggerIDs[i]] = {state and 1 or 0}
+                    outputTriggers[extraTriggerIDs[i]] = {state}
                 end
             end
         end,
@@ -624,7 +651,7 @@ local function multiStickySwitch(p)
     
     local triggerID = p.triggerID
     local stateTriggerID = p.stateTriggerID
-
+	
     local justPressed = {}
     local justReleased = {}
 
@@ -644,7 +671,7 @@ local function multiStickySwitch(p)
             end
         end,
         updateState = function(self, departures, triggerArrivals)
-            state = true
+			state = true
             for i = 1, bCount do
                 if triggerArrivals[stateTriggerID][i] == 0 then
                     state = false
@@ -660,7 +687,7 @@ local function multiStickySwitch(p)
                         state = false
                     end
                     justPressed[i] = triggerArrivals[stateTriggerID][i] == 0 and individualState[i]
-                    justReleased[i] = triggerArrivals[stateTriggerID][i] == 1 and not individualState[i]
+                    justReleased[i] = triggerArrivals[stateTriggerID][i] > 0 and not individualState[i]
                 end
             else
                 for i = 1, bCount do
@@ -668,10 +695,15 @@ local function multiStickySwitch(p)
                     justReleased[i] = false
                 end
             end
+			
+			state = (state and triggerArrivals[triggerID][1] + 1) or 0
+			if state > 2 then
+				state = 2
+			end
         end,
         calculateGlitz = function(self, forwardsGlitz, reverseGlitz, persistentGlitz)
             for i = 1, bCount do
-                local forGlitz, revGlitz = calculateButtonGlitz(proto[i], PnV[i], state or individualState[i])
+                local forGlitz, revGlitz = calculateButtonGlitz(proto[i], PnV[i], (state > 0) or individualState[i])
                 table.insert(forwardsGlitz, forGlitz)
                 table.insert(reverseGlitz, revGlitz)
                 if justPressed[i] then
@@ -687,12 +719,12 @@ local function multiStickySwitch(p)
             end
         end,
         fillTrigger = function(self, outputTriggers)
-            outputTriggers[triggerID] = {state and 1 or 0}
+            outputTriggers[triggerID] = {state}
             outputTriggers[stateTriggerID] = map(function(val) return val and 1 or 0 end, individualState)
             if p.extraTriggerIDs then
                 local extraTriggerIDs = p.extraTriggerIDs
                 for i = 1, #extraTriggerIDs do
-                    outputTriggers[extraTriggerIDs[i]] = {state and 1 or 0}
+                    outputTriggers[extraTriggerIDs[i]] = {state}
                 end
             end
         end,
@@ -745,7 +777,7 @@ local function toggleSwitch(p)
         calculateGlitz = function(self, forwardsGlitz, reverseGlitz, persistentGlitz)
             do
                 local forGlitz, revGlitz =
-                    calculateButtonGlitz(constructCompleteProto(timeDirection, self.first), firstPnV, switchState == 1)
+                    calculateButtonGlitz(constructCompleteProto(timeDirection, self.first), firstPnV, switchState > 0)
                 table.insert(forwardsGlitz, forGlitz)
                 table.insert(reverseGlitz, revGlitz)
             end
@@ -995,7 +1027,7 @@ local function wireGlitz(p)
             local forGlitz, revGlitz
             local active = false
             if triggerID then
-                active = (useTriggerArrival and triggerArrivals[triggerID][1] == 1) or (not useTriggerArrival and outputTriggers[triggerID][1] == 1)
+                active = (useTriggerArrival and triggerArrivals[triggerID][1] > 0) or (not useTriggerArrival and outputTriggers[triggerID][1] > 0)
             elseif triggerFunction then
                 active = triggerFunction(triggerArrivals, outputTriggers)
             end
@@ -1097,7 +1129,15 @@ function calculatePhysicsAffectingStuff(tempStore)
         
         retv.mutators, tempStore.activeMutators = calculateMutators(tempStore.protoMutators, retv.collisions, triggerArrivals)
         
-        local portals, arrivalLocations = calculatePortals(tempStore.forwardsGlitz, tempStore.reverseGlitz, tempStore.protoPortals, retv.collisions, triggerArrivals)
+        local portals, arrivalLocations = calculatePortals(
+			tempStore.forwardsGlitz, 
+			tempStore.reverseGlitz, 
+			tempStore.protoPortals, 
+			tempStore.protoLasers, 
+			retv.collisions, 
+			triggerArrivals, 
+			tempStore.frameNumber
+		)
         
         retv.portals = portals
         retv.arrivalLocations = arrivalLocations

data/levels/0Level.lvl/main.lua
@@ -1,6 +1,6 @@
 name = "Guy Killer"
 speedOfTime = 3
-timelineLength = 500
+timelineLength = 5000
 environment =
 {
     gravity = 22,
@@ -18,7 +18,7 @@ environment =
         {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
         {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1},
         {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1},
-        {1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1},
+        {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1},
         {1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1},
         {1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1},
         {1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1},
@@ -80,5 +80,5 @@ triggerSystem =
             default = {12 * 3200, 12 * 3200, 0, 0}
         },
     },
-    arrivalLocationsSize = 1,
+    arrivalLocationsSize = 2,
 }

data/levels/0Level.lvl/triggerSystem.lua
@@ -21,6 +21,28 @@ local tempStore =
             winner = true
         },
     },
+    protoLasers =
+    {
+        {
+            attachment = {xOffset = 6 * 3200, yOffset = 6 * 3200},
+            index = 2,
+            xaim = 5 * 3200,
+            yaim = 20 * 3200,
+            collisionOverlap = 50,
+            timeDirection = 'forwards',
+            destinationIndex = nil,
+            xDestination = 0,
+            yDestination = 0,
+            relativeTime = true,
+            timeDestination = -600,
+            illegalDestination = 1,
+            fallable = false,
+            winner = false,
+			triggerFunction = function(triggerArrivals, frameNumber)
+				return triggerArrivals[1][1] == 1
+			end
+        },
+    },
     protoCollisions = {
         {
             width = 3 * 3200,

src/DirectLuaTriggerSystem.cpp
@@ -189,8 +189,7 @@ PortalArea toPortal(lua_State *L, std::size_t arrivalLocationsSize)
     
     int x(readField<int>(L, "x"));
     int y(readField<int>(L, "y"));
-    int width(readField<int>(L, "width"));
-    int height(readField<int>(L, "height"));
+
     int xspeed(readField<int>(L, "xspeed"));
     int yspeed(readField<int>(L, "yspeed"));
     int collisionOverlap(readField<int>(L, "collisionOverlap"));
@@ -234,12 +233,25 @@ PortalArea toPortal(lua_State *L, std::size_t arrivalLocationsSize)
     lua_pop(L, 1);
     
     bool fallable(readField<bool>(L, "fallable"));
+    bool isLaser(readField<bool>(L, "isLaser"));
     bool winner(readField<bool>(L, "winner"));
-    
+    int xaim = 0;
+    int yaim = 0;
+    int width = 0;
+    int height = 0;
+    if (isLaser) {
+        xaim = readField<int>(L, "xaim");
+        yaim = readField<int>(L, "yaim");
+    }
+    else {
+        width = readField<int>(L, "width");
+        height = readField<int>(L, "height");
+    }
     return
         PortalArea(
             index,
             x, y,
+            xaim, yaim,
             width, height,
             xspeed, yspeed,
             collisionOverlap,
@@ -251,6 +263,7 @@ PortalArea toPortal(lua_State *L, std::size_t arrivalLocationsSize)
             destinationDirection,
             illegalDestination,
             fallable,
+            isLaser,
             winner);
 }
 

src/PhysicsEngineUtilities_def.h
@@ -101,11 +101,12 @@ template <
     // fall through portals
     for (unsigned i = 0; i < portals.size(); ++i)
     {
-        if (IntersectingRectanglesInclusiveCollisionOverlap(x, y, size, size,
+        if (portals[i].getFallable() &&
+            (!portals[i].getIsLaser()) &&
+            IntersectingRectanglesInclusiveCollisionOverlap(x, y, size, size,
             portals[i].getX(), portals[i].getY(),
             portals[i].getWidth(), portals[i].getHeight(),
-            portals[i].getCollisionOverlap())
-            && portals[i].getFallable())
+            portals[i].getCollisionOverlap()))
         {
             if (oldIllegalPortal != -1 && portals[i].getIndex() == oldIllegalPortal)
             {
@@ -1027,7 +1028,6 @@ void guyStep(
                                 carry[i] = true;
                                 carrySize[i] = boxSize;
                                 carryDirection[i] = nextBoxIt->object.getTimeDirection();
-
                                 nextBoxIt = nextBox.erase(nextBoxIt);
                                 nextBoxNormalDepartureIt = nextBoxNormalDeparture.erase(nextBoxNormalDepartureIt);
                                 nextBoxEnd = nextBox.end();
@@ -1162,12 +1162,13 @@ void guyStep(
             {
                 for (unsigned int j = 0; j < nextPortal.size(); ++j)
                 {
-                    if (IntersectingRectanglesInclusiveCollisionOverlap(
+                    if (nextPortal[j].getFallable() && 
+                        (!nextPortal[j].getIsLaser()) &&
+                        IntersectingRectanglesInclusiveCollisionOverlap(
                         x[i], y[i], newWidth[i], newHeight[i],
                         nextPortal[j].getX(), nextPortal[j].getY(),
                         nextPortal[j].getWidth(), nextPortal[j].getHeight(),
-                        nextPortal[j].getCollisionOverlap())
-                        && nextPortal[j].getFallable())
+                        nextPortal[j].getCollisionOverlap()))
                     {
                         if (guyArrivalList[i].getIllegalPortal() != -1 && nextPortal[j].getIndex() == guyArrivalList[i].getIllegalPortal())
                         {
@@ -1282,7 +1283,8 @@ void guyStep(
                 {
                     for (unsigned int j = 0; j < nextPortal.size(); ++j)
                     {
-                        if (IntersectingRectanglesInclusiveCollisionOverlap(
+                        if ((!nextPortal[j].getIsLaser()) &&
+                            IntersectingRectanglesInclusiveCollisionOverlap(
                             x[i], y[i], newWidth[i], newHeight[i],
                             nextPortal[j].getX(), nextPortal[j].getY(),
                             nextPortal[j].getWidth(), nextPortal[j].getHeight(),
@@ -1400,6 +1402,205 @@ void guyStep(
             assert(guyArrivalList[i].getIndex() == playerInput.size());
             nextPlayerFrame = true;
             finishedWith[i] = true;
+        }
+    }
+
+    // Get shot by laser portals
+    for (unsigned int i = 0; i < nextPortal.size(); ++i)
+    {
+        std::size_t numberOfGuys = guyArrivalList.size();
+        if (nextPortal[i].getIsLaser())
+        {
+            // Make map of guys which are legal to shoot. Illegal ones are invisible to raytrace
+            mt::std::vector<char> shootable;
+            shootable.reserve(boost::size(guyArrivalList));
+
+            for (std::size_t j(0); j != numberOfGuys; ++j)
+            {
+                shootable.push_back(!finishedWith[j] && i != j && !newTimePaused[i]);
+            }
+
+            // Return values from doGunRaytrace
+            PhysicsObjectType targetType = NONE;
+            std::size_t targetId = std::numeric_limits<std::size_t>::max();
+
+            int sx = nextPortal[i].getX();
+            int sy = nextPortal[i].getY();
+            int px = nextPortal[i].getXaim();
+            int py = nextPortal[i].getYaim();
+
+            doGunRaytrace(
+                targetType, targetId, env,
+                sx, sy, px, py,
+                nextPlatform,
+                nextBox, nextBoxNormalDeparture,
+                x, y, newWidth, newHeight,
+                shootable);
+
+            // Should be a different glitz adder?
+            guyGlitzAdder.addLaserGlitz(sx, sy, px, py, sx, sy, nextPortal[i].getTimeDirection());
+
+            if (targetType == GUY)
+            {
+                assert(targetId != std::numeric_limits<std::size_t>::max());
+                assert(targetId >= 0 && targetId < boost::size(guyArrivalList));
+
+                TimeDirection nextTimeDirection = guyArrivalList[targetId].getTimeDirection();
+                Frame *nextTime(nextFrame(frame, nextTimeDirection));
+
+                if (nextPortal[i].getWinner())
+                {
+                    winFrame = true;
+                    nextTime = nullptr;
+                }
+                else
+                {
+                    Frame *portalTime(
+                        nextPortal[i].getRelativeTime() ?
+                        getArbitraryFrame(
+                            getUniverse(frame),
+                            getFrameNumber(frame) + nextPortal[i].getTimeDestination()) :
+                        getArbitraryFrame(
+                            getUniverse(frame),
+                            nextPortal[i].getTimeDestination()));
+
+
+                    if (nextPortal[i].getRelativeDirection())
+                    {
+                        nextTimeDirection *= nextPortal[i].getDestinationDirection();
+                    }
+                    else
+                    {
+                        nextTimeDirection = nextPortal[i].getDestinationDirection();
+                    }
+
+                    nextTime = portalTime ? nextFrame(portalTime, nextTimeDirection) : nullptr;
+                    illegalPortal[targetId] = nextPortal[i].getIllegalDestination();
+                    int arrivalBasis = nextPortal[i].getDestinationIndex();
+                    if (arrivalBasis != -1)
+                    {
+                        x[targetId] = x[targetId] - nextPortal[i].getX() + nextPortal[i].getXdestination();
+                        y[targetId] = y[targetId] - nextPortal[i].getY() + nextPortal[i].getYdestination();
+                        if (nextPortal[i].getTimeDirection() * nextTimeDirection == TimeDirection::FORWARDS)
+                        {
+                            xspeed[targetId] = xspeed[targetId] - nextPortal[i].getXspeed();
+                            yspeed[targetId] = yspeed[targetId] - nextPortal[i].getYspeed();
+                        }
+                        else
+                        {
+                            xspeed[targetId] = xspeed[targetId] + nextPortal[i].getXspeed();
+                            yspeed[targetId] = yspeed[targetId] + nextPortal[i].getYspeed();
+                        }
+                    }
+                    else
+                    {
+                        x[targetId] = x[targetId] + nextPortal[i].getXdestination();
+                        y[targetId] = y[targetId] + nextPortal[i].getYdestination();
+                    }
+                }
+
+                nextGuy.push_back(
+                    ObjectAndTime<Guy, Frame *>(
+                        Guy(
+                            guyArrivalList[targetId].getIndex() + 1,
+                            x[targetId], y[targetId],
+                            xspeed[targetId], yspeed[targetId],
+                            newWidth[targetId], newHeight[targetId],
+                            newJumpSpeed[targetId],
+
+                            illegalPortal[targetId],
+                            -1,
+                            supported[targetId],
+                            supportedSpeed[targetId],
+
+                            newPickups[targetId],
+                            facing[targetId],
+
+                            carry[targetId],
+                            carrySize[targetId],
+                            carryDirection[targetId],
+
+                            guyArrivalList[targetId].getTimeDirection(),
+                            newTimePaused[targetId]
+                            ),
+                        nextTime
+                        )
+                    );
+
+                finishedWith[targetId] = true;
+            }
+            else if (targetType == BOX)
+            {
+                assert(targetId != std::numeric_limits<std::size_t>::max());
+                assert(targetId >= 0 && targetId < boost::size(nextBox));
+
+                int boxX = nextBox[targetId].object.getX();
+                int boxY = nextBox[targetId].object.getY();
+                int boxXspeed = nextBox[targetId].object.getXspeed();
+                int boxYspeed = nextBox[targetId].object.getYspeed();
+                int size = nextBox[targetId].object.getSize();
+
+                TimeDirection timeDirection = nextBox[targetId].object.getTimeDirection();
+
+                Frame *portalTime(
+                    nextPortal[i].getRelativeTime() ?
+                    getArbitraryFrame(getUniverse(frame), getFrameNumber(frame) + nextPortal[i].getTimeDestination()) :
+                    getArbitraryFrame(getUniverse(frame), nextPortal[i].getTimeDestination()));
+
+                if (nextPortal[i].getRelativeDirection())
+                {
+                    timeDirection *= nextPortal[i].getDestinationDirection();
+                }
+                else
+                {
+                    timeDirection = nextPortal[i].getDestinationDirection();
+                }
+
+                Frame *nextTime = !isNullFrame(portalTime) ? nextFrame(portalTime, timeDirection) : nullptr;
+                int arrivalBasis = nextPortal[i].getDestinationIndex();
+
+                if (arrivalBasis != -1)
+                {
+                    boxX = boxX - nextPortal[i].getX() + nextPortal[i].getXdestination();
+                    boxY = boxY - nextPortal[i].getY() + nextPortal[i].getYdestination();
+                    if (nextPortal[i].getTimeDirection() * timeDirection == TimeDirection::FORWARDS)
+                    {
+                        boxXspeed = boxXspeed - nextPortal[i].getXspeed();
+                        boxYspeed = boxYspeed - nextPortal[i].getYspeed();
+                    }
+                    else
+                    {
+                        boxXspeed = boxXspeed + nextPortal[i].getXspeed();
+                        boxYspeed = boxYspeed + nextPortal[i].getYspeed();
+                    }
+                }
+                else
+                {
+                    boxX = boxX + nextPortal[i].getXdestination();
+                    boxY = boxY + nextPortal[i].getYdestination();
+                }
+
+                // Replace box
+                assert(targetId < nextBox.size());
+
+                nextBox[targetId] = ObjectAndTime<Box, Frame *>(
+                        Box(
+                            boxX,
+                            boxY,
+                            boxXspeed,
+                            boxYspeed,
+                            size,
+                            nextPortal[i].getIllegalDestination(),
+                            arrivalBasis,
+                            timeDirection),
+                        nextTime);
+                nextBoxNormalDeparture[targetId] = false;
+            }
+            else
+            {
+                assert(targetType == NONE);
+                assert(targetId == std::numeric_limits<std::size_t>::max());
+            }
         }
     }
 

commit f5dcbedc06f5bf3162a82eb3da8112d49fe243e5

Fixed bug with stickyLaserSwitch introduced by 76864b139396cc380a3b2b88020bf7d6464216eb The stickyLaserSwitch was reading the incoming trigger state in an incorrect way that would cause the incoming state to always be interpreted as indicating that the switch had already been triggered. This caused stickyLaserSwitch to immediately become stuck in the on state.

data/basicTriggerSystem.lua
@@ -606,7 +606,7 @@ local function stickyLaserSwitch(p)
         end,
         updateState = function(self, departures, triggerArrivals)
 			local oldState = triggerArrivals[stateTriggerID][1]
-            state = (oldState and oldState + 1) or (checkPressed(constructDynamicArea(proto, PnV), departures) and 1) or 0
+            state = (oldState > 0 and oldState + 1) or (checkPressed(constructDynamicArea(protoBeam, beamPnV), departures) and 1) or 0
 			if state > 2 then
 				state = 2
 			end