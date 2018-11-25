185ffb360e42ab60faab513ac27dee457a53f929
GoogleFrog <googl3frog@gmail.com>
2018-11-25 18:35:33 +1100

    Added level Retcon as a simple example of a level that requires possession.

---
+++ data/levels/Retcon.lvl/main.lua
@@ -0,0 +1,70 @@
+name = "Retcon"
+speedOfTime = 1
+timelineLength = 5400
+environment =
+{
+    gravity = 10,
+    wall =
+    {
+        width = 16,
+        height = 15,
+        segmentSize = 3200,
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,},
+        {1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,},
+        {1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,},
+        {1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,},
+        {1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,},
+        {1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,},
+        {1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,},
+        {1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,},
+        {1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,},
+        {1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,},
+        {1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,},
+        {1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,},
+        {1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,},
+        {1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,},
+    }
+}
+initialGuy =
+{
+    arrivalTime = 0,
+    arrival =
+    {
+        x = 6.75 * 3200,
+        y = 3 * 3200,
+        width = 1600,
+        height = 3200,
+        facing = "right",
+        timeDirection = "forwards",
+        pickups = {timeJump = 0, timeReverse = 0, timeGun = 0}
+    }
+}
+initialArrivals =
+{
+    {type = "box", x = 7.5 * 3200, y = 10 * 3200, xspeed = 0, yspeed = 0, size = 3200, timeDirection = "forwards"},
+}
+triggerSystem =
+{
+    type = "DirectLua",
+    
+    luaFiles = {"triggerSystem", "global.basicTriggerSystem"},
+    system = [==[ require "triggerSystem" ]==],
+
+    -- C++ still manages offsets and defaults.
+    triggerOffsetsAndDefaults = {
+        {
+            offset = 1,
+            default = {1}
+        },
+        {
+            offset = 1,
+            default = {0}
+        },
+        {
+            offset = 1,
+            default = {3 * 3200, 7 * 3200, 0, 0}
+        },
+    },
+    arrivalLocationsSize = 1
+}
