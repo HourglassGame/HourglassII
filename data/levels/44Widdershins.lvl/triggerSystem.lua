ee0f5869cd7a5811ac37ac735c831e1df3b956d4
GoogleFrog <googl3frog@gmail.com>
2018-11-26 14:07:08 +1100

    Finished Widdershins.

--- data/levels/44Widdershins.lvl/triggerSystem.lua
+++ data/levels/44Widdershins.lvl/triggerSystem.lua
@@ -5,7 +5,7 @@ local tempStore =
     protoPortals =
     {
         {
-            attachment = {xOffset = 13 * 3200, yOffset = 4 * 3200},
+            attachment = {xOffset = 13.5 * 3200, yOffset = 4 * 3200},
             index = 1,
             width = 2 * 3200,
             height = 2 * 3200,
@@ -60,10 +60,48 @@ local tempStore =
                 }
             }
         },
+        {
+            width = 2 * 3200,
+            height = 1 * 3200,
+            timeDirection = 'forwards',
+            lastStateTriggerID = 10,
+            buttonTriggerID = 9,
+            destinations =
+            {
+                onDestination = {
+                    xDestination = {
+                        desiredPosition = 1 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 20,
+                        deceleration = 20
+                    },
+                    yDestination = {
+                        desiredPosition = 15 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 20,
+                        deceleration = 20
+                    }
+                },
+                offDestination = {
+                    xDestination = {
+                        desiredPosition = 1 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 20,
+                        deceleration = 20
+                    },
+                    yDestination = {
+                        desiredPosition = 9 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 20,
+                        deceleration = 20
+                    }
+                }
+            }
+        },
     },
     protoMutators = {
         bts.pickup{
-            attachment = {platform = nil, xOffset = 13.5 * 3200, yOffset = 9 * 3200},
+            attachment = {platform = nil, xOffset = 13.5 * 3200, yOffset = 10.25 * 3200},
             width = 1600,
             height = 1600,
             timeDirection = 'forwards',
@@ -71,7 +109,7 @@ local tempStore =
             pickupType = 'timeJump'
         },
         bts.pickup{
-            attachment = {platform = nil, xOffset = 14.5 * 3200, yOffset = 9 * 3200},
+            attachment = {platform = nil, xOffset = 14.5 * 3200, yOffset = 10.25 * 3200},
             width = 1600,
             height = 1600,
             timeDirection = 'forwards',
@@ -79,11 +117,35 @@ local tempStore =
             pickupType = 'timeGun'
         },
         bts.pickup{
-            attachment = {platform = nil, xOffset = 15.5 * 3200, yOffset = 9 * 3200},
+            attachment = {platform = nil, xOffset = 15.5 * 3200, yOffset = 10.25 * 3200},
             width = 1600,
             height = 1600,
             timeDirection = 'forwards',
             triggerID = 5,
+            pickupType = 'timeGun'
+        },
+        bts.pickup{
+            attachment = {platform = nil, xOffset = 4.25 * 3200, yOffset = 14.25 * 3200},
+            width = 1600,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 6,
+            pickupType = 'timeJump'
+        },
+        bts.pickup{
+            attachment = {platform = nil, xOffset = 3.25 * 3200, yOffset = 14.25 * 3200},
+            width = 1600,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 7,
+            pickupType = 'timeReverse'
+        },
+        bts.pickup{
+            attachment = {platform = nil, xOffset = 3.75 * 3200, yOffset = 13.25 * 3200},
+            width = 1600,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 8,
             pickupType = 'timeGun'
         },
     },
@@ -94,6 +156,13 @@ local tempStore =
             height = 1600,
             timeDirection = 'forwards',
             triggerID = 1,
+        },
+        bts.stickySwitch{
+            attachment = {xOffset = 6.75 * 3200, yOffset = 14 * 3200},
+            width = 800,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 9,
         },
     },
     protoGlitz = {
