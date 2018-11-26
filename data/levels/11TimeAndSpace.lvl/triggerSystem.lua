d98be5ba191782c7954437129c612d63edca4b19
GoogleFrog <googl3frog@gmail.com>
2018-11-26 18:56:13 +1100

    Added levels 11 and 12.

---
+++ data/levels/11TimeAndSpace.lvl/triggerSystem.lua
@@ -0,0 +1,109 @@
+local bts = require "global.basicTriggerSystem"
+
+local tempStore =
+{
+    protoPortals =
+    {
+        {
+            attachment = {platform = 1, xOffset = 0.5 * 3200, yOffset = -2 * 3200},
+            index = 1,
+            width = 2 * 3200,
+            height = 2 * 3200,
+            collisionOverlap = 50,
+            timeDirection = 'forwards',
+            destinationIndex = 1,
+            xDestination = 0,
+            yDestination = 0,
+            relativeTime = true,
+            timeDestination = -6 * 60,
+            illegalDestination = 1,
+            fallable = false,
+            winner = false
+        },
+        {
+            attachment = {platform = nil, xOffset = 2.5 * 3200, yOffset = 8 * 3200},
+            index = 1,
+            width = 2 * 3200,
+            height = 2 * 3200,
+            collisionOverlap = 50,
+            timeDirection = 'forwards',
+            destinationIndex = 1,
+            xDestination = 0,
+            yDestination = 0,
+            relativeTime = false,
+            timeDestination = 0,
+            illegalDestination = 1,
+            fallable = false,
+            winner = true
+        }
+    },
+    protoCollisions = {
+        {
+            width = 3 * 3200,
+            height = 1 * 3200,
+            timeDirection = 'forwards',
+            lastStateTriggerID = 2,
+            buttonTriggerID = 1,
+            destinations =
+            {
+                onDestination = {
+                    xDestination = {
+                        desiredPosition = 22 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 10,
+                        deceleration = 10
+                    },
+                    yDestination = {
+                        desiredPosition = 10 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 10,
+                        deceleration = 10
+                    }
+                },
+                offDestination = {
+                    xDestination = {
+                        desiredPosition = 7 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 10,
+                        deceleration = 10
+                    },
+                    yDestination = {
+                        desiredPosition = 10 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 10,
+                        deceleration = 10
+                    }
+                }
+            }
+        },
+    },
+    protoMutators = {
+        bts.spikes{
+            attachment = {platform = nil, xOffset = 10 * 3200, yOffset = 17.5 * 3200},
+            width = 12 * 3200,
+            height = 0.5 * 3200,
+            timeDirection = 'forwards',
+        },
+    },
+    protoButtons = {
+        bts.stickySwitch{
+            attachment = {platform = nil, xOffset = 28.5 * 3200, yOffset = 9.75 * 3200},
+            width = 1600,
+            height = 800,
+            timeDirection = 'forwards',
+            triggerID = 1
+        },
+    }
+}
+
+calculatePhysicsAffectingStuff = bts.calculatePhysicsAffectingStuff(tempStore)
+
+function shouldArrive(dynamicObject)
+    return true
+end
+function shouldPort(responsiblePortalIndex, dynamicObject, porterActionedPortal)
+    return true
+end
+mutateObject = bts.mutateObject(tempStore)
+
+getDepartureInformation = bts.getDepartureInformation(tempStore)
---
+++ data/levels/12FairlyRoutine.lvl/triggerSystem.lua
@@ -0,0 +1,133 @@
+local bts = require "global.basicTriggerSystem"
+
+local tempStore =
+{
+    protoPortals =
+    {
+        {
+            attachment = {platform = nil, xOffset = 26.5 * 3200, yOffset = 7 * 3200},
+            index = 1,
+            width = 2 * 3200,
+            height = 2 * 3200,
+            collisionOverlap = 50,
+            timeDirection = 'forwards',
+            destinationIndex = 1,
+            xDestination = 0,
+            yDestination = 0,
+            relativeTime = false,
+            timeDestination = 0,
+            illegalDestination = 1,
+            fallable = false,
+            winner = true
+        }
+    },
+    protoCollisions = {
+        {
+            width = 2 * 3200,
+            height = 1 * 3200,
+            timeDirection = 'forwards',
+            lastStateTriggerID = 3,
+            buttonTriggerID = 2,
+            destinations =
+            {
+                onDestination = {
+                    xDestination = {
+                        desiredPosition = 10 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 10,
+                        deceleration = 10
+                    },
+                    yDestination = {
+                        desiredPosition = 10 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 10,
+                        deceleration = 10
+                    }
+                },
+                offDestination = {
+                    xDestination = {
+                        desiredPosition = 10 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 10,
+                        deceleration = 10
+                    },
+                    yDestination = {
+                        desiredPosition = 8 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 10,
+                        deceleration = 10
+                    }
+                }
+            }
+        },
+        {
+            width = 2 * 3200,
+            height = 1 * 3200,
+            timeDirection = 'forwards',
+            lastStateTriggerID = 4,
+            buttonTriggerID = 2,
+            destinations =
+            {
+                onDestination = {
+                    xDestination = {
+                        desiredPosition = 9 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 10,
+                        deceleration = 10
+                    },
+                    yDestination = {
+                        desiredPosition = 11 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 10,
+                        deceleration = 10
+                    }
+                },
+                offDestination = {
+                    xDestination = {
+                        desiredPosition = 9 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 10,
+                        deceleration = 10
+                    },
+                    yDestination = {
+                        desiredPosition = 9 * 3200,
+                        maxSpeed = 200,
+                        acceleration = 10,
+                        deceleration = 10
+                    }
+                }
+            }
+        }
+    },
+    protoMutators = {
+        bts.pickup{
+            attachment = {platform = nil, xOffset = 5.25 * 3200, yOffset = 11.25 * 3200},
+            width = 1600,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 1,
+            pickupType = 'timeJump'
+        },
+    },
+    protoButtons = {
+        bts.stickySwitch{
+            attachment = {platform = nil, xOffset = 21.75 * 3200, yOffset = 11 * 3200},
+            width = 800,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 2
+        },
+    }
+}
+
+calculatePhysicsAffectingStuff = bts.calculatePhysicsAffectingStuff(tempStore)
+
+function shouldArrive(dynamicObject)
+    return true
+end
+function shouldPort(responsiblePortalIndex, dynamicObject, porterActionedPortal)
+    return true
+end
+mutateObject = bts.mutateObject(tempStore)
+
+getDepartureInformation = bts.getDepartureInformation(tempStore)
