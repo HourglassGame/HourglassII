d84e9735a4a1f884dab0bceb99c98f41abefa790
Evan Wallace <mankarse.git@alethrea.net>
2018-12-09 15:51:51 +1100

    Added basic (MSVC only, for now) cmake project definition. Deleted all old project defintions, in the future, they should be generated from the CMake definition.

--- ci_supervisor_osx.py
+++
@@ -1,5 +0,0 @@
-import imp
-while True:
-    import ci_agent_osx
-    ci_agent_osx.main()
-    imp.reload(ci_agent_osx)
--- ci_supervisor_windows.py
+++
@@ -1,5 +0,0 @@
-import imp
-while True:
-    import ci_agent_windows
-    ci_agent_windows.main()
-    imp.reload(ci_agent_windows)
