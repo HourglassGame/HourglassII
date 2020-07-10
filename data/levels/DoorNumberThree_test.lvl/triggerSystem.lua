dbf98b5876aefbcf344d4474a2e13d7d5fd4520b
GoogleFrog <googl3frog@gmail.com>
2020-07-10 15:57:53 +1000

    Added a short replay for the raytrace bug.

---
+++ data/levels/DoorNumberThree_test.lvl/triggerSystem.lua
@@ -0,0 +1,201 @@
+local bts = require "global.basicTriggerSystem"
+
+local tempStore =
+{
+    protoPortals =
+    {
+        {
+            attachment = {platform = nil, xOffset = 26 * 3200, yOffset = 15 * 3200},
+            index = 1,
+            width = 2 * 3200,
+            height = 2 * 3200,
+            collisionOverlap = 50,
+            timeDirection = 'forwards',
+            destinationIndex = 1,
+            xDestination = 0,
+            yDestination = 0,
+            relativeTime = false,
+            timeDestination = 0,
+            illegalDestination = 1,
+            fallable = false,
+            winner = true,
+        }
+    },
+    protoCollisions = {
+        {
+            width = 3200,
+            height = 1 * 3200,
+            timeDirection = 'forwards',
+            lastStateTriggerID = 7,
+            buttonTriggerID = 1,
+            destinations =
+            {
+                onDestination = {
+                    xDestination = {
+                        desiredPosition = 9 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    },
+                    yDestination = {
+                        desiredPosition = 0 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    }
+                },
+                offDestination = {
+                    xDestination = {
+                        desiredPosition = 9 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    },
+                    yDestination = {
+                        desiredPosition = 4 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    }
+                }
+            }
+        },
+        {
+            width = 3200,
+            height = 1 * 3200,
+            timeDirection = 'forwards',
+            lastStateTriggerID = 8,
+            buttonTriggerID = 2,
+            destinations =
+            {
+                onDestination = {
+                    xDestination = {
+                        desiredPosition = 16 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    },
+                    yDestination = {
+                        desiredPosition = 3 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    }
+                },
+                offDestination = {
+                    xDestination = {
+                        desiredPosition = 16 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    },
+                    yDestination = {
+                        desiredPosition = 7 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    }
+                }
+            }
+        },
+        {
+            width = 3200,
+            height = 1 * 3200,
+            timeDirection = 'forwards',
+            lastStateTriggerID = 9,
+            buttonTriggerID = 3,
+            destinations =
+            {
+                onDestination = {
+                    xDestination = {
+                        desiredPosition = 23 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    },
+                    yDestination = {
+                        desiredPosition = 6 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    }
+                },
+                offDestination = {
+                    xDestination = {
+                        desiredPosition = 23 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    },
+                    yDestination = {
+                        desiredPosition = 10 * 3200,
+                        maxSpeed = 300,
+                        acceleration = 50,
+                        deceleration = 50
+                    }
+                }
+            }
+        }
+    },
+    protoMutators = {
+        bts.pickup{
+            attachment = {platform = nil, xOffset = 5.75 * 3200, yOffset = 6.25 * 3200},
+            width = 1600,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 4,
+            pickupType = 'timeJump'
+        },
+        bts.pickup{
+            attachment = {platform = nil, xOffset = 12.75 * 3200, yOffset = 9.25 * 3200},
+            width = 1600,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 5,
+            pickupType = 'timeJump'
+        },
+        bts.pickup{
+            attachment = {platform = nil, xOffset = 19.75 * 3200, yOffset = 12.25 * 3200},
+            width = 1600,
+            height = 1600,
+            timeDirection = 'forwards',
+            triggerID = 6,
+            pickupType = 'timeGun'
+        }
+    },
+    protoButtons = {
+        bts.momentarySwitch{
+            attachment = {platform = nil, xOffset = 5.5 * 3200, yOffset = 7.75 * 3200},
+            width = 3200,
+            height = 800,
+            timeDirection = 'forwards',
+            triggerID = 1
+        },
+        bts.momentarySwitch{
+            attachment = {platform = nil, xOffset = 12.5 * 3200, yOffset = 10.75 * 3200},
+            width = 3200,
+            height = 800,
+            timeDirection = 'forwards',
+            triggerID = 2
+        },
+        bts.momentarySwitch{
+            attachment = {platform = nil, xOffset = 19.5 * 3200, yOffset = 13.75 * 3200},
+            width = 3200,
+            height = 800,
+            timeDirection = 'forwards',
+            triggerID = 3
+        },
+    }
+}
+
+calculatePhysicsAffectingStuff = bts.calculatePhysicsAffectingStuff(tempStore)
+
+function shouldArrive(dynamicObject)
+    return true
+end
+function shouldPort(responsiblePortalIndex, dynamicObject, porterActionedPortal)
+    return true
+end
+mutateObject = bts.mutateObject(tempStore)
+
+getDepartureInformation = bts.getDepartureInformation(tempStore)
